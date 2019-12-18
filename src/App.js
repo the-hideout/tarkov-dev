136ecddefd7f4174423babfc32acdac1c87e94e7
Oskar Risberg <oskar.risberg@stendahls.se>
2019-12-18 09:35:03 +0100

    Better naming and type detection

--- src/App.js
+++ src/App.js
@@ -4,60 +4,37 @@ import { VictoryChart, VictoryScatter, VictoryTheme } from 'victory';
 import './App.css';
 import data from './data.json';
 
-function getType(name) {
+function getTypeAndName(name) {
+    
     if(name.includes('.366')) {
-        return '0.366';
+        return {
+            type: '0.366',
+            name: name.replace( '.366 ', '' ),
+        };
     }
     
-    if(name.includes('5.45x39')) {
-        return '5.45x39mm';
+    if(name.includes('12/70')) {
+        return {
+            type: '12/70',
+            name: name.replace( '12/70 ', '' ),
+        };
     }
     
-    if(name.includes('5.45x39')) {
-        return '5.45x39mm';
+    if(name.includes('20/70')) {
+        return {
+            type: '20/70',
+            name: name.replace( '20/70 ', '' ),
+        };
     }
     
-    if(name.includes('5.56x45')) {
-        return '5.56x45mm';
+    const matches = name.match( /\d{1,2}(\.\d{1,2})?x\d*(\s?mm)?R?/ );
+    
+    return {
+        type: matches[ 0 ],
+        name: name.replace( `${matches[ 0 ]}`, '' ).trim(),
+    };
     }
     
-    if(name.includes('5.7x28')) {
-        return '5.7x28mm';
-    }
-    
-    if(name.includes('7.62x25')) {
-        return '7.62x25mm';
-    }
-    
-    if(name.includes('7.62x39')) {
-        return '7.62x39mm';
-    }
-    
-    if(name.includes('7.62x51')) {
-        return '7.62x51mm';
-    }
-    
-    if(name.includes('7.62x54R')) {
-        return '7.62x54R';
-    }
-    
-    if(name.includes('9x18')) {
-        return '9x18mm';
-    }
-    
-    if(name.includes('9x19')) {
-        return '9x19mm';
-    }
-    
-    if(name.includes('9x21')) {
-        return '9x21mm';
-    }
-    
-    if(name.includes('9x39')) {
-        return '9x39mm';
-    }
-}
-
 function parseData(){
     return data.map((ammoRow) => {
         if(!ammoRow.Damage){
@@ -67,8 +44,7 @@ function parseData(){
         return {
             ...ammoRow,
             'Penetration Value': Number(ammoRow['Penetration Value']),
-            type: getType(ammoRow['0.12 Patch']),
-            name: ammoRow['0.12 Patch'],
+            ...getTypeAndName(ammoRow['0.12 Patch']),
         };
     }).filter(Boolean);
 }
@@ -87,39 +63,38 @@ function App() {
                 <VictoryScatter
                     symbol={
                         ({ datum }) => {
-                            if (datum.type === '9x39mm'){
+                            if (datum.type.indexOf('7.62x54R') === 0){
                                 return 'triangleUp';
                             }
                             
-                            if (datum.type === '9x21mm'){
+                            if (datum.type.indexOf('7.62') === 0){
                                 return 'diamond';
                             }
                             
-                            if (datum.type === '9x19mm'){
-                                return 'triangleDown';
-                            }
-                            
-                            if (datum.type === '9x18mm'){
-                                return 'circle';
-                            }
-                            
-                            if (datum.type === '7.62x54R'){
-                                return 'plus';
-                            }
-                            
-                            if (datum.type === '7.62x51mm'){
-                                return 'minus';
-                            }
-                            
-                            if (datum.type === '7.62x39mm'){
-                                return 'square';
-                            }
-                            
-                            if (datum.type === '7.62x25mm'){
+                            if (datum.type.indexOf('5.56') === 0){
                                 return 'star';
                             }
                             
-                            return 'triangleDown';
+                            // Same icon, should be different color
+                            if (datum.type.indexOf('5.45') === 0){
+                                return 'star';
+                            }
+                            
+                            if (datum.type.indexOf('9x') === 0){
+                                return 'plus';
+                            }
+                            
+                            if (datum.type.indexOf('12.7') === 0){
+                                return 'triangleDown';
+                            }
+                            
+                            if (datum.type.indexOf('12/70') === 0 || datum.type.indexOf('20/70') === 0){
+                                return 'circle';
+                            }
+                            
+                            console.log(`No symbol defined for ${datum.type}`);
+                            
+                            return 'minus';
                         }
                     }
                     style={{
