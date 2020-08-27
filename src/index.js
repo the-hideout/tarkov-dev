d43fb087c8b982faa2bb4a6a7ea717a5107d3757
Oskar Risberg <oskar.risberg@stendahls.se>
2020-08-27 09:12:20 +0200

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {
     BrowserRouter as Router,
-  } from "react-router-dom";
+} from "react-router-dom";
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+    dsn: "https://0c31a9f171194843bd9ae061c0856470@o90181.ingest.sentry.io/5407213",
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
+    tracesSampleRate: 1.0,
+  });
 
 ReactDOM.render((<Router><App /></Router>), document.getElementById('root'));
 
