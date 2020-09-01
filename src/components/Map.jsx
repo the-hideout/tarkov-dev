b87adb2e7ba6bd2b66762067db756301cb77bf14
Oskar Risberg <oskar.risberg@stendahls.se>
2020-09-01 13:58:50 +0200

    Add zoom for maps!

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
     useParams,
-  } from "react-router-dom";
+} from "react-router-dom";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 import rawMapData from '../map-data.json';
 
@@ -18,12 +19,21 @@ const maps = Object.fromEntries(rawMapData.map((mapData) => {
 function Map() {
     let {currentMap} = useParams();
     
-    return <img 
-        alt = {`Map of ${maps[currentMap].displayText}`}
-        className = "map-image"
-        title = {`Map of ${maps[currentMap].displayText}`}
-        src = {`${process.env.PUBLIC_URL}${maps[currentMap].image}`}
-    />
+    return <TransformWrapper
+        defaultScale={1}
+        wheel = {{
+            step: 200,
+        }}
+    >
+        <TransformComponent>
+            <img 
+                alt = {`Map of ${maps[currentMap].displayText}`}
+                className = "map-image"
+                title = {`Map of ${maps[currentMap].displayText}`}
+                src = {`${process.env.PUBLIC_URL}${maps[currentMap].image}`}
+            />
+        </TransformComponent>
+    </TransformWrapper>;
 }
 
 export default Map;
