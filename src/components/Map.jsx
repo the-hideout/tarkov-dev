66f71258e40c4ca3f609273631c7b3db21c0625c
kokarn <kokarn@gmail.com>
2020-09-30 18:44:20 +0200

    Add map-time. Closes #13

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,6 +4,8 @@ import {
 } from "react-router-dom";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
+import Time from './Time';
+
 import rawMapData from '../map-data.json';
 
 const maps = Object.fromEntries(rawMapData.map((mapData) => {
@@ -21,25 +23,28 @@ function Map() {
 
     const { displayText,image } = maps[currentMap];
 
-    return <TransformWrapper
-        defaultScale={1}
-        wheel = {{
-            step: 200,
-        }}
-    >
-        <TransformComponent>
-            <div
-                className = "map-image-wrapper"
-            >
-                <img
-                    alt = {`Map of ${ displayText }`}
-                    className = "map-image"
-                    title = {`Map of ${ displayText }`}
-                    src = {`${ process.env.PUBLIC_URL }${ image }`}
-                />
-            </div>
-        </TransformComponent>
-    </TransformWrapper>;
+    return <div>
+        <Time />
+        <TransformWrapper
+            defaultScale={1}
+            wheel = {{
+                step: 200,
+            }}
+        >
+            <TransformComponent>
+                <div
+                    className = "map-image-wrapper"
+                >
+                    <img
+                        alt = {`Map of ${ displayText }`}
+                        className = "map-image"
+                        title = {`Map of ${ displayText }`}
+                        src = {`${ process.env.PUBLIC_URL }${ image }`}
+                    />
+                </div>
+            </TransformComponent>
+        </TransformWrapper>
+    </div>;
 }
 
 export default Map;
---
+++ src/components/Time.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import dayjs from 'dayjs';
+import dayjsUtc from 'dayjs/plugin/utc'
+
+import useDate from '../hooks/useDate';
+
+/*
+Huge thanks to Adam Burgess
+https://github.com/adamburgess/tarkov-time
+as most of the code is his.
+
+Thanks a bunch!
+*/
+
+dayjs.extend(dayjsUtc);
+
+// 1 second real time = 7 seconds tarkov time
+const tarkovRatio = 7;
+
+export function hrs(num) {
+    return 1000 * 60 * 60 * num;
+}
+
+export function realTimeToTarkovTime(time, left) {
+    // tarkov time moves at 7 seconds per second.
+    // surprisingly, 00:00:00 does not equal unix 0... but it equals unix 10,800,000.
+    // Which is 3 hours. What's also +3? Yep, Russia. UTC+3.
+    // therefore, to convert real time to tarkov time,
+    // tarkov time = (real time * 7 % 24 hr) + 3 hour
+
+    const oneDay = hrs(24);
+    const russia = hrs(3);
+
+    const offset = russia + (left ? 0 : hrs(12));
+    const tarkovTime = new Date((offset + (time.getTime() * tarkovRatio)) % oneDay);
+
+    return tarkovTime;
+};
+
+export function timeUntilRelative(until, left, date) {
+    const tarkovTime = realTimeToTarkovTime(date, left);
+
+    if (until < tarkovTime.getTime()) {
+        until += hrs(24);
+    }
+
+    const diffTarkov = until - tarkovTime.getTime();
+    const diffRT = diffTarkov / tarkovRatio;
+
+    return diffRT;
+};
+
+export function formatFuture(ms) {
+    const time = dayjs.utc(ms);
+    const hour = time.hour();
+    const min = time.minute();
+    const sec = time.second();
+    let text = '';
+
+    if (hour != 0) {
+        text = hour + 'hr';
+    }
+
+    text += min + 'min';
+
+    if(hour == 0 && min == 0) {
+        text = sec + 's';
+    }
+
+    return text;
+}
+
+function Time() {
+    const time = useDate(new Date(), 50);
+    const tarkovTime1 = realTimeToTarkovTime(time, true);
+    const tarkovTime2 = realTimeToTarkovTime(time);
+
+    return <div
+        className = 'time-wrapper'
+    >
+        <div>
+            {dayjs.utc(tarkovTime1).format('HH:mm:ss')}
+        </div>
+        <div>
+            {dayjs.utc(tarkovTime2).format('HH:mm:ss')}
+        </div>
+    </div>;
+}
+
+export default Time;
