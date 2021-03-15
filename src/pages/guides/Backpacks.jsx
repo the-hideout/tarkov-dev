860a4735b3dd577e78e1e523bf8d49c49a6de646
kokarn <kokarn@gmail.com>
2021-03-15 16:16:19 +0100

    Add a canvas grid component

--- src/pages/guides/Backpacks.jsx
+++ src/pages/guides/Backpacks.jsx
@@ -1,12 +1,98 @@
 import {useMemo} from 'react';
 import {Helmet} from 'react-helmet';
 
+import CanvasGrid from '../../components/canvas-grid';
 import DataTable from '../../components/data-table';
 import items from '../../Items';
 import formatPrice from '../../modules/format-price';
 import ID from '../../components/ID.jsx';
 
 let displayItems = [];
+
+const NOTES = {
+    '5e4abc6786f77406812bd572': 'Can only keep medical items',
+};
+
+const GRID_MAP = {
+    '5c0e805e86f774683f3dd637': [ // Paratus
+        {
+            row: 0,
+            col: 0,
+            width: 5,
+            height: 5,
+        },
+        {
+            row: 5,
+            col: 0,
+            width: 1,
+            height: 2,
+        },
+        {
+            row: 5,
+            col: 1,
+            width: 3,
+            height: 2,
+        },
+        {
+            row: 5,
+            col: 4,
+            width: 1,
+            height: 2,
+        },
+    ],
+    '5f5e46b96bdad616ad46d613': [ // Eberlestock
+        {
+            row: 0,
+            col: 0,
+            width: 5,
+            height: 4,
+        },
+        {
+            row: 4,
+            col: 0,
+            width: 5,
+            height: 2,
+        },
+        {
+            row: 6,
+            col: 0,
+            width: 5,
+            height: 2,
+        },
+    ],
+    '5d5d940f86f7742797262046': [ // Mechanism
+        {
+            row: 0,
+            col: 0,
+            width: 4,
+            height: 4,
+        },
+        {
+            row: 4,
+            col: 0,
+            width: 2,
+            height: 2,
+        },
+        {
+            row: 4,
+            col: 2,
+            width: 2,
+            height: 2,
+        },
+        {
+            row: 6,
+            col: 0,
+            width: 2,
+            height: 2,
+        },
+        {
+            row: 6,
+            col: 2,
+            width: 2,
+            height: 2,
+        },
+    ],
+};
 
 for(const item of Object.values(items)){
     if(!item.types.includes('backpack')){
@@ -32,10 +118,6 @@ const priceCell = ({ value }) => {
     </div>;
 };
 
-const NOTES = {
-    '5e4abc6786f77406812bd572': 'Can only keep medical items',
-};
-
 function Backpacks(props) {
     const columns = useMemo(
         () => [
@@ -54,6 +136,30 @@ function Backpacks(props) {
                 },
             },
             {
+                Header: 'Grid',
+                accessor: 'grid',
+                Cell: ({value}) => {
+                    let grid = [{
+                        row: 0,
+                        col: 0,
+                        width: value.width,
+                        height: value.height,
+                    }];
+
+                    if(GRID_MAP[value.id]){
+                        grid = GRID_MAP[value.id];
+                    }
+
+                    return <div>
+                        <CanvasGrid
+                            height = {value.height}
+                            grid = {grid}
+                            width = {value.width}
+                        />
+                    </div>
+                },
+            },
+            {
                 Header: 'Name',
                 accessor: 'name',
                 Cell: (cellData) => {
@@ -67,6 +173,7 @@ function Backpacks(props) {
                             {cellData.value}
                         </a>
                         {NOTES[fullItemData.id] ? <cite>{NOTES[fullItemData.id]}</cite> : ''}
+                        {/* {JSON.stringify(fullItemData.grid.pockets, null, 4)} */}
                     </div>
                 },
             },
@@ -113,16 +220,22 @@ function Backpacks(props) {
         }
 
         return {
+            grid: {
+                id: item.id,
+                height: item.itemProperties.grid.totalSize / item.itemProperties.grid.pockets[0].width,
+                width: item.itemProperties.grid.pockets[0].width,
+                pockets: item.itemProperties.grid.pockets,
+            },
             id: item.id,
+            image: `https://assets.tarkov-tools.com/${item.id}-icon.jpg`,
             name: itemName,
+            price: item.price,
+            pricePerSlot: Math.floor(item.price / item.slots),
+            ratio: (item.itemProperties.grid?.totalSize / item.slots).toFixed(2),
             size: item.itemProperties.grid?.totalSize,
             slots: item.slots,
             speedPenalty: `${item.itemProperties.speedPenaltyPercent || 0}%`,
-            image: `https://assets.tarkov-tools.com/${item.id}-icon.jpg`,
-            price: item.price,
             wikiLink: item.wikiLink,
-            ratio: (item.itemProperties.grid?.totalSize / item.slots).toFixed(2),
-            pricePerSlot: Math.floor(item.price / item.slots),
         };
     })
     .filter(Boolean), [])
