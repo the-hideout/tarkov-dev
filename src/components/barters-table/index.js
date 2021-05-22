5c04fd7d2258d7ab1edfed36326f119026f6c1dc
kokarn <kokarn@gmail.com>
2021-05-22 20:43:09 +0200

    Add trader & loyalty selector to barters thanks to @Hiltuska ✺◟( ͡° ͜ʖ ͡°)◞✺ Closes #56

--- src/components/barters-table/index.js
+++ src/components/barters-table/index.js
@@ -54,7 +54,7 @@ function costItemsCell({ value }) {
                     <div
                         className = 'price-wrapper'
                     >
-                        <span className = 'barter-cost-item-count-wrapper'>{costItem.count}</span> X {formatPrice(costItem.avg24hPrice)} = {formatPrice(costItem.count * costItem.avg24hPrice)}
+                        <span className = 'barter-cost-item-count-wrapper'>{costItem.count}</span> X {formatPrice(costItem.avg24hPrice)} = {formatPrice(costItem.count * costItem.avg24hPrice)}
                         {/* {formatPrice(costItem.avg24hPrice)} */}
                     </div>
                 </div>
@@ -64,7 +64,7 @@ function costItemsCell({ value }) {
 };
 
 function BartersTable(props) {
-    const {nameFilter} = props;
+    const { selectedTrader, nameFilter, levelFilter = 3} = props;
     const [barters, setBarters] = useState([]);
 	const { query } = useQuery`{
         barters {
@@ -206,114 +206,151 @@ function BartersTable(props) {
         []
     )
 
-    const data = useMemo(() => barters.map((barterRow) => {
-        let cost = 0;
+    const data = useMemo(() => {
+        let addedTraders = [];
 
-        if(!barterRow.rewardItems[0]){
-            console.log(barterRow);
-            return false;
-        }
+        return barters.map((barterRow) => {
+            let cost = 0;
 
-        if(nameFilter.length > 0){
-            let matchesFilter = false;
-            const findString = nameFilter.toLowerCase().replace(/\s/g, '');
-            for(const requiredItem of barterRow.requiredItems){
-                if(requiredItem === null){
-                    continue;
-                }
-
-                if(requiredItem.item.name.toLowerCase().replace(/\s/g, '').includes(findString)){
-                    matchesFilter = true;
-
-                    break;
-                }
-            }
-
-            for(const rewardItem of barterRow.rewardItems){
-                if(rewardItem.item.name.toLowerCase().replace(/\s/g, '').includes(findString)){
-                    matchesFilter = true;
-
-                    break;
-                }
-            }
-
-            if(!matchesFilter){
+            if(!barterRow.rewardItems[0]){
+                console.log(barterRow);
                 return false;
             }
-        }
 
-        // let hasZeroCostItem = false;
-
-        const tradeData = {
-            costItems: barterRow.requiredItems.map(requiredItem => {
+            if(nameFilter.length > 0){
+                let matchesFilter = false;
+                const findString = nameFilter.toLowerCase().replace(/\s/g, '');
+                for(const requiredItem of barterRow.requiredItems){
                     if(requiredItem === null){
-                        // console.log(barterRow);
-
-                        return false;
+                        continue;
                     }
 
-                    if(!requiredItem.item){
-                        // console.log(requiredItem);
+                    if(requiredItem.item.name.toLowerCase().replace(/\s/g, '').includes(findString)){
+                        matchesFilter = true;
 
-                        return false;
+                        break;
                     }
+                }
 
-                    const requiredItemPrice = requiredItem.item.avg24hPrice || Math.max(...requiredItem.item.traderPrices.map(priceObject => priceObject.price))
+                for(const rewardItem of barterRow.rewardItems){
+                    if(rewardItem.item.name.toLowerCase().replace(/\s/g, '').includes(findString)){
+                        matchesFilter = true;
 
-                    cost = cost + requiredItemPrice * requiredItem.count;
+                        break;
+                    }
+                }
 
-                    // if(cost === 0){
-                    //     hasZeroCostItem = true;
-                    // }
+                if(!matchesFilter){
+                    return false;
+                }
+            }
 
-                    return {
-                        count: requiredItem.count,
-                        name: requiredItem.item.name,
-                        avg24hPrice: requiredItemPrice,
-                        iconLink: requiredItem.item.iconLink,
-                        wikiLink: requiredItem.item.wikiLink,
-                        itemLink: `/item/${requiredItem.item.normalizedName}`,
-                    };
-                })
-                .filter(Boolean),
-            cost: cost,
-            reward: {
-                name: barterRow.rewardItems[0].item.name,
-                wikiLink: barterRow.rewardItems[0].item.wikiLink,
-                value: barterRow.rewardItems[0].item.avg24hPrice || Math.max(...barterRow.rewardItems[0].item.traderPrices.map(priceObject => priceObject.price)),
-                trader: barterRow.source,
-                iconLink: barterRow.rewardItems[0].item.iconLink,
-                itemLink: `/item/${barterRow.rewardItems[0].item.normalizedName}`,
-            },
-        };
+            // let hasZeroCostItem = false;
+            let [trader, level] = barterRow.source.split('LL');
 
-        tradeData.savings = tradeData.reward.value - cost;
+            level = parseInt(level);
+            trader = trader.trim();
 
-        // If the reward has no value, it's not available for purchase
-        if(tradeData.reward.value === 0){
-            tradeData.reward.value = tradeData.cost;
-            tradeData.reward.barterOnly = true;
-            tradeData.savings = 0;
-        }
+            if(!nameFilter && selectedTrader && selectedTrader !== 'all' && selectedTrader !== trader.toLowerCase().replace(/\s/g, '-')){
+                return false;
+            }
 
-        // if(hasZeroCostItem){
-        //     return false;
-        // }
+            if(level > levelFilter.value){
+                return false;
+		    }
 
-        return tradeData;
-    })
-    .filter(Boolean)
-    .sort((itemA, itemB) => {
-        if(itemB.savings > itemA.savings){
-            return -1;
-        };
+            console.log(barterRow)
+            const tradeData = {
+                costItems: barterRow.requiredItems.map(requiredItem => {
+                        if(requiredItem === null){
+                            // console.log(barterRow);
 
-        if(itemB.savings < itemA.savings){
-            return 1;
-        }
+                            return false;
+                        }
 
-        return 0;
-    }), [nameFilter, barters]);
+                        if(!requiredItem.item){
+                            // console.log(requiredItem);
+
+                            return false;
+                        }
+
+                        const requiredItemPrice = requiredItem.item.avg24hPrice || Math.max(...requiredItem.item.traderPrices.map(priceObject => priceObject.price))
+
+                        cost = cost + requiredItemPrice * requiredItem.count;
+
+                        // if(cost === 0){
+                        //     hasZeroCostItem = true;
+                        // }
+
+                        return {
+                            count: requiredItem.count,
+                            name: requiredItem.item.name,
+                            avg24hPrice: requiredItemPrice,
+                            iconLink: requiredItem.item.iconLink,
+                            wikiLink: requiredItem.item.wikiLink,
+                            itemLink: `/item/${requiredItem.item.normalizedName}`,
+                        };
+                    })
+                    .filter(Boolean),
+                cost: cost,
+                reward: {
+                    name: barterRow.rewardItems[0].item.name,
+                    wikiLink: barterRow.rewardItems[0].item.wikiLink,
+                    value: barterRow.rewardItems[0].item.avg24hPrice || Math.max(...barterRow.rewardItems[0].item.traderPrices.map(priceObject => priceObject.price)),
+                    trader: barterRow.source,
+                    iconLink: barterRow.rewardItems[0].item.iconLink,
+                    itemLink: `/item/${barterRow.rewardItems[0].item.normalizedName}`,
+                },
+            };
+
+            tradeData.savings = tradeData.reward.value - cost;
+
+            // If the reward has no value, it's not available for purchase
+            if(tradeData.reward.value === 0){
+                tradeData.reward.value = tradeData.cost;
+                tradeData.reward.barterOnly = true;
+                tradeData.savings = 0;
+            }
+
+            // if(hasZeroCostItem){
+            //     return false;
+            // }
+
+            return tradeData;
+        })
+        .filter(Boolean)
+        .sort((itemA, itemB) => {
+            if(itemB.savings > itemA.savings){
+                return -1;
+            };
+
+            if(itemB.savings < itemA.savings){
+                return 1;
+            }
+
+            return 0;
+        })
+        .filter((barter) => {
+
+            if(selectedTrader !== 'all'){
+                return true;
+		    }
+
+            if(selectedTrader === 'all'){
+                return true;
+			}
+
+            if(addedTraders.includes(barter.reward.source)){
+                return false;
+		    }
+
+            addedTraders.push(barter.reward.source);
+
+            return true;
+	    });
+    },
+        [nameFilter, levelFilter, selectedTrader, barters]
+    );
 
     if(data.length <= 0){
         return <div>
--- src/pages/barters/index.js
+++ src/pages/barters/index.js
@@ -1,16 +1,52 @@
 import {useState} from 'react';
 import {Helmet} from 'react-helmet';
+import Select from 'react-select';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
 
 import BartersTable from '../../components/barters-table';
+import useStateWithLocalStorage from '../../hooks/useStateWithLocalStorage'
 
 import Icon from '@mdi/react'
 import { mdiAccountSwitch } from '@mdi/js';
 
 import './index.css';
 
+const levels = [
+    { value: 1, label: '1'},
+    { value: 2, label: '2'},
+    { value: 3, label: '3'},
+    { value: 4, label: '4'},
+]
+
+const traders = [
+    'prapor',
+    'therapist',
+    'skier',
+    'peacekeeper',
+    'mechanic',
+    'jaeger',
+];
+
+function capitalizeTheFirstLetterOfEachWord(words) {
+    const separateWord = words.toLowerCase().split(' ');
+
+    for (let i = 0; i < separateWord.length; i = i + 1) {
+       separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+       separateWord[i].substring(1);
+    }
+
+    return separateWord.join(' ');
+}
+
 function Barters() {
     const defaultQuery = new URLSearchParams(window.location.search).get('search');
-    const [nameFilter, setNameFilter] = useState(defaultQuery || '');
+    const [nameFilter, setNameFilter] = useState(defaultQuery || '');
+    const [levelFilter, setLevelFilter] = useState(levels[3]);
+    const [levelTooltipDisabled, setLevelTooltipDisabled] = useState(false);
+    const hideLevelTooltip = () => setLevelTooltipDisabled(true);
+    const showLevelTooltip = () => setLevelTooltipDisabled(false);
+    const [selectedTrader, setSelectedTrader] = useStateWithLocalStorage('selectedTrader', 'all');
 
     return [
         <Helmet
@@ -41,6 +77,42 @@ function Barters() {
                 />
                 Barter profits
             </h1>
+            <div className = 'button-group-wrapper'>
+                {traders.map((traderName) => {
+                    return <Tippy
+                        placement = 'top'
+                        content={
+                        <div>
+                            {capitalizeTheFirstLetterOfEachWord(traderName.replace('-', ' '))}
+                        </div>
+					}>
+                        <button
+                            className = {`button-group-button ${traderName === selectedTrader ? 'selected': ''}`}
+                            key = {`trader-selector-button-${traderName}`}
+                            onClick={setSelectedTrader.bind(undefined, traderName)}
+                        ><img
+                            alt = {traderName}
+                            title = {traderName}
+                            src={`${process.env.PUBLIC_URL}/images/${traderName}-icon.jpg`}
+                        /></button>
+                    </Tippy>
+				})}
+                <Tippy
+                    placement = 'top'
+                    content={
+                    <div>
+                        Show all barters
+                    </div>
+                }>
+                    <button
+                        className = {`button-group-button ${'all' === selectedTrader ? 'selected': ''}`}
+                        title = 'All Traders Barters'
+                        onClick={setSelectedTrader.bind(undefined, 'all')}
+                    >
+                        All
+                    </button>
+                </Tippy>
+            </div>
             <div
                 className = 'filter-input-wrapper'
             >
@@ -50,15 +122,46 @@ function Barters() {
                     Item filter
                 </div>
                 <input
-                    defaultValue = {nameFilter || ''}
+                    defaultValue = {nameFilter || ''}
                     type = {'text'}
                     placeholder = {'filter on item'}
                     onChange = {e => setNameFilter(e.target.value)}
                 />
             </div>
+            <Tippy
+                disabled = {levelTooltipDisabled}
+                placement = 'bottom'
+                content={
+                    <div>
+                        Sets maximum loyalty level for traders
+                    </div>
+                }
+            >
+                <div
+                    className = 'filter-dropdown-wrapper'
+                >
+                    <div
+                        className = 'filter-dropdown-label'
+                    >
+                        Loyalty
+                    </div>
+                    <Select
+                        defaultValue={levelFilter}
+                        name="levels"
+                        onChange={setLevelFilter}
+                        options={levels}
+                        className="filter-dropdown-select"
+                        classNamePrefix="select"
+                        onMenuOpen={hideLevelTooltip}
+                        onMenuClose={showLevelTooltip}
+                    />
+                </div>
+            </Tippy>
         </div>,
         <BartersTable
+            levelFilter = {levelFilter}
             nameFilter = {nameFilter}
+            selectedTrader = {selectedTrader}
             key = 'barters-page-barters-table'
         />
     ];
