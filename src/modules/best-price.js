1ced69ff6187dd7ec804e3f2c93b2cb90e9f1666
kokarn <kokarn@gmail.com>
2021-05-26 13:18:27 +0200

    quickfix loading times

--- src/modules/best-price.js
+++ src/modules/best-price.js
@@ -1,26 +1,26 @@
 const calculateFee = require('./flea-market-fee');
 
 module.exports = (itemData) => {
-    let currentFee = calculateFee(itemData.basePrice, itemData.avg24hPrice);
-    let bestProfit = itemData.avg24hPrice - currentFee;
+    let currentFee = calculateFee(itemData.avg24hPrice, itemData.basePrice);
+    // let bestProfit = itemData.avg24hPrice - currentFee;
     let bestPrice = itemData.avg24hPrice;
-    let bestPriceFee = currentFee;
-    for(let i = itemData.avg24hPrice - 100; i > 0; i = i - 100){
-        const newFee = calculateFee(i, itemData.basePrice);
+    // let bestPriceFee = currentFee;
+    // for(let i = itemData.avg24hPrice - 1000; i > 0; i = i - 1000){
+    //     const newFee = calculateFee(i, itemData.basePrice);
 
-        const newProfit = i - newFee;
+    //     const newProfit = i - newFee;
 
-        if(newProfit <= bestProfit){
-            break;
-        }
+    //     if(newProfit <= bestProfit){
+    //         break;
+    //     }
 
-        bestPrice = i;
-        bestProfit = newProfit;
-        bestPriceFee = newFee;
-    }
+    //     bestPrice = i;
+    //     bestProfit = newProfit;
+    //     bestPriceFee = newFee;
+    // }
 
     return {
         bestPrice: bestPrice,
-        bestPriceFee: bestPriceFee,
+        bestPriceFee: currentFee,
     };
 };
