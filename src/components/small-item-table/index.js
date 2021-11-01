8eceebe422d8ff5bd04ccc71ca16a92db34c041e
kokarn <kokarn@gmail.com>
2021-11-01 11:13:16 +0100

    sort with better matches first :D

--- src/components/small-item-table/index.js
+++ src/components/small-item-table/index.js
@@ -103,7 +103,7 @@ function SmallItemTable(props) {
             const fuse = new Fuse(returnData, options);
             const result = fuse.search(nameFilter);
 
-              returnData = result.sort((a, b) => b.score - a.score).map(resultObject => resultObject.item);
+            returnData = result.sort((a, b) => a.score - b.score).map(resultObject => resultObject.item);
         }
 
         if(defaultRandom && !nameFilter){
