a3f417a9d9c59f2c4dc8cc15626539630861f3ae
kokarn <kokarn@gmail.com>
2021-12-07 08:25:00 +0100

    scroll to top

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import store from './store';
 import { Provider } from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
 
+import ScrollToTop from './components/scroll-to-top';
+
 if(window.location.hostname !== 'localhost' && window.location.protocol !== 'file:'){
     Sentry.init({
         dsn: 'https://0c31a9f171194843bd9ae061c0856470@o90181.ingest.sentry.io/5407213',
@@ -23,6 +25,7 @@ ReactDOM.render((
         store = {store}
     >
         <Router>
+            <ScrollToTop />
             <App />
         </Router>
     </Provider>
