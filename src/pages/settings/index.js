3af13f1361c684220d4debf2734bf23a9c1665bd
kokarn <kokarn@gmail.com>
2021-12-17 21:59:01 +0100

    don't change on ref change

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -98,6 +98,7 @@ function Settings() {
                 });
             }
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [useTarkovTracker, allStations]);
 
     return <div
