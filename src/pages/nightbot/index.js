3cb6737b58153e4cde15cdbf82e10539f3c7febd
kokarn <kokarn@gmail.com>
2021-12-20 20:42:16 +0100

    fix DOM nesting

--- src/pages/nightbot/index.js
+++ src/pages/nightbot/index.js
@@ -13,55 +13,55 @@ function Nightbot() {
         <h2>
             Instructions
         </h2>
+        <ul>
+            <li>
+                Register at <a href="https://nightbot.tv">nightbot.tv</a> using your twitch / youtube account
+            </li>
+            <li>
+                Go to dashboard <a href="https://nightbot.tv/dashboard">nightbot.tv/dashboard</a>
+            </li>
+            <li>
+                Click the "Join Channel" button
+            </li>
+        </ul>
         <p>
-            <ul>
-                <li>
-                    Register at <a href="https://nightbot.tv">nightbot.tv</a> using your twitch / youtube account
-                </li>
-                <li>
-                    Go to dashboard <a href="https://nightbot.tv/dashboard">nightbot.tv/dashboard</a>
-                </li>
-                <li>
-                    Click the "Join Channel" button
-                </li>
-            </ul>
             <img
                 alt = {'Nightbot step 1'}
                 src = {`${process.env.PUBLIC_URL}/images/nightbot-1.jpg`}
             />
         </p>
+        <ul>
+            <li>
+                Make bot - moderator, just type /mod nightbot in your chat
+            </li>
+            <li>
+                Go to custom commands <a href="https://nightbot.tv/commands/custom">nightbot.tv/commands/custom</a>
+            </li>
+            <li>
+                Press the "Add command" button
+            </li>
+        </ul>
         <p>
-            <ul>
-                <li>
-                    Make bot - moderator, just type /mod nightbot in your chat
-                </li>
-                <li>
-                    Go to custom commands <a href="https://nightbot.tv/commands/custom">nightbot.tv/commands/custom</a>
-                </li>
-                <li>
-                    Press the "Add command" button
-                </li>
-            </ul>
             <img
                 alt = {'Nightbot step 2'}
                 src = {`${process.env.PUBLIC_URL}/images/nightbot-2.jpg`}
             />
         </p>
+        <ul>
+            <li>
+                Command: !p or anything you like
+            </li>
+            <li>
+                Message:
+                <pre>
+                    $(urlfetch https://tarkov-tools.com/webhook/nightbot?q=$(querystring))
+                </pre>
+            </li>
+            <li>
+                Press "Submit"
+            </li>
+        </ul>
         <p>
-            <ul>
-                <li>
-                    Command: !p or anything you like
-                </li>
-                <li>
-                    Message:
-                    <pre>
-                        $(urlfetch https://tarkov-tools.com/webhook/nightbot?q=$(querystring))
-                    </pre>
-                </li>
-                <li>
-                    Press "Submit"
-                </li>
-            </ul>
             <img
                 alt = {'Nightbot step 3'}
                 src = {`${process.env.PUBLIC_URL}/images/nightbot-3.jpg`}
--- src/pages/stream-elements/index.js
+++ src/pages/stream-elements/index.js
@@ -13,55 +13,55 @@ function StreamElements() {
         <h2>
             Instructions
         </h2>
+        <ul>
+            <li>
+                Register at <a href="https://streamelements.com/">streamelements.com</a> using your twitch / youtube account
+            </li>
+            <li>
+                Go to dashboard <a href="https://streamelements.com/dashboard">streamelements.com/dashboard</a>
+            </li>
+            <li>
+                Click the "Join Channel" button
+            </li>
+        </ul>
         <p>
-            <ul>
-                <li>
-                    Register at <a href="https://streamelements.com/">streamelements.com</a> using your twitch / youtube account
-                </li>
-                <li>
-                    Go to dashboard <a href="https://streamelements.com/dashboard">streamelements.com/dashboard</a>
-                </li>
-                <li>
-                    Click the "Join Channel" button
-                </li>
-            </ul>
             <img
                 alt = 'StreamElements step 1'
                 src = {`${process.env.PUBLIC_URL}/images/streamelements-1.jpg`}
             />
         </p>
+        <ul>
+            <li>
+                Make bot - moderator, just type /mod streamelements in your chat
+            </li>
+            <li>
+                Go to custom commands <a href="https://streamelements.com/dashboard/bot/commands/custom">streamelements.com/dashboard/bot/commands/custom</a>
+            </li>
+            <li>
+                Press the "Add new command" button
+            </li>
+        </ul>
         <p>
-            <ul>
-                <li>
-                    Make bot - moderator, just type /mod streamelements in your chat
-                </li>
-                <li>
-                    Go to custom commands <a href="https://streamelements.com/dashboard/bot/commands/custom">streamelements.com/dashboard/bot/commands/custom</a>
-                </li>
-                <li>
-                    Press the "Add new command" button
-                </li>
-            </ul>
             <img
                 alt = 'StreamElements step 2'
                 src = {`${process.env.PUBLIC_URL}/images/streamelements-2.jpg`}
             />
         </p>
+        <ul>
+            <li>
+                Command: !p or anything you like
+            </li>
+            <li>
+                Message:
+                <pre>
+                    $(urlfetch https://tarkov-tools.com/webhook/stream-elements?q=$(querystring))
+                </pre>
+            </li>
+            <li>
+                Press "Activate Command"
+            </li>
+        </ul>
         <p>
-            <ul>
-                <li>
-                    Command: !p or anything you like
-                </li>
-                <li>
-                    Message:
-                    <pre>
-                        $(urlfetch https://tarkov-tools.com/webhook/stream-elements?q=$(querystring))
-                    </pre>
-                </li>
-                <li>
-                    Press "Activate Command"
-                </li>
-            </ul>
             <img
                 alt = 'StreamElements part 3'
                 src = {`${process.env.PUBLIC_URL}/images/streamelements-3.jpg`}
