539aa3702eb48768526d57471becd5f48f2dc5aa
Jawsbreaker <evansrichardmichael@gmail.com>
2021-12-21 22:06:27 -0700

    working on styling multi select

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Switch from 'react-switch';
 import Select from 'react-select';
 import Slider from 'rc-slider';
@@ -14,177 +14,208 @@ const ConditionalWrapper = ({ condition, wrapper, children }) => {
     return condition ? wrapper(children) : children;
 };
 
-function ButtonGroupFilterButton ({tooltipContent, onClick, content, selected}) {
+function ButtonGroupFilterButton({ tooltipContent, onClick, content, selected }) {
     return <Tippy
-        placement = 'top'
+        placement='top'
         content={tooltipContent}
     >
         <button
-            className = {`button-group-button ${selected ? 'selected': ''}`}
+            className={`button-group-button ${selected ? 'selected' : ''}`}
 
             onClick={onClick}
         >
             {content}
         </button>
-    </Tippy>
+    </Tippy>;
 };
 
-function ButtonGroupFilter ({children}) {
+function ButtonGroupFilter({ children }) {
     return <div
-        className = 'button-group-wrapper'
+        className='button-group-wrapper'
     >
         {children}
-    </div>
+    </div>;
 };
 
-function SliderFilter ({label, defaultValue, min, max, marks, onChange, reverse = false}) {
+function SliderFilter({ label, defaultValue, min, max, marks, onChange, reverse = false }) {
     return <div
-        className = {'filter-slider-wrapper'}
+        className={'filter-slider-wrapper'}
     >
         <div
-            className = {'filter-slider-label'}
+            className={'filter-slider-label'}
         >
             {label}
         </div>
         <Slider
-            defaultValue = {defaultValue}
-            min = {min}
-            max = {max}
-            marks = {marks}
-            onChange = {onChange}
-            trackStyle = {{
+            defaultValue={defaultValue}
+            min={min}
+            max={max}
+            marks={marks}
+            onChange={onChange}
+            trackStyle={{
                 backgroundColor: '#048802',
             }}
-            handleStyle = {{
-                backgroundColor: '#048802',
-                borderColor: '#048802',
-            }}
-            activeDotStyle = {{
+            handleStyle={{
                 backgroundColor: '#048802',
                 borderColor: '#048802',
             }}
-            reverse = {reverse}
-            style = {{
+            activeDotStyle={{
+                backgroundColor: '#048802',
+                borderColor: '#048802',
+            }}
+            reverse={reverse}
+            style={{
                 top: '-7px',
                 width: '170px',
             }}
         />
-    </div>
+    </div>;
 };
 
-function ToggleFilter ({label, onChange, checked, tooltipContent, disabled}) {
+function ToggleFilter({ label, onChange, checked, tooltipContent, disabled }) {
     return <ConditionalWrapper
-        condition = {tooltipContent}
-        wrapper = {
+        condition={tooltipContent}
+        wrapper={
             children => {
                 return <Tippy
-                    placement = 'bottom'
-                    content = {tooltipContent}
+                    placement='bottom'
+                    content={tooltipContent}
                 >
                     {children}
-                </Tippy>
-            }
-        }
-        >
-        <label
-            className = {'single-filter-wrapper'}
-        >
-            <span
-                className = {'single-filter-label'}
-            >
-                {label}
-            </span>
-            <Switch
-                className = {'filter-toggle'}
-                onChange = {onChange}
-                checked = {checked}
-                disabled = {disabled}
-            />
-        </label>
-    </ConditionalWrapper>
-};
-
-function SelectFilter ({defaultValue, options, onChange, isMulti = false, label, tooltip, tooltipDisabled, onMenuOpen, onMenuClose, wide, parentRef}) {
-    return <ConditionalWrapper
-        condition = {tooltip}
-        wrapper = {
-            children => {
-                return <Tippy
-                    disabled = {tooltipDisabled}
-                    placement = 'bottom'
-                    content = {tooltip}
-                >
-                    {children}
-                </Tippy>
+                </Tippy>;
             }
         }
     >
         <label
-            className = {`single-filter-wrapper ${wide ? 'single-filter-wrapper-wide' : ''}`}
+            className={'single-filter-wrapper'}
         >
             <span
-                className = {'single-filter-label'}
+                className={'single-filter-label'}
             >
                 {label}
             </span>
-            <Select
-                className = "basic-multi-select"
-                classNamePrefix = "select"
-                defaultValue = {defaultValue}
-                isMulti = {isMulti}
-                name = "colors"
-                onChange = {onChange}
-                onMenuClose = {onMenuClose}
-                onMenuOpen = {onMenuOpen}
-                options = {options}
-                ref = {parentRef}
+            <Switch
+                className={'filter-toggle'}
+                onChange={onChange}
+                checked={checked}
+                disabled={disabled}
             />
         </label>
     </ConditionalWrapper>;
 };
 
-function InputFilter ({defaultValue, type, placeholder, onChange, label}) {
+const selectFilterStyle = {
+
+    menu: (provided) => ({
+        ...provided,
+
+        backgroundColor: '#292626',
+        border: '2px solid #9a8866'
+
+    }),
+    control: (provided) => ({
+        ...provided,
+        backgroundColor: '#292626',
+        border: '2px solid #9a8866'
+
+    }),
+    menuList: (provided) => ({
+        ...provided,
+        color: '#E5E5E5',
+
+    }),
+    //TODO I think I am unable to use options because they dont exist yet
+    options: (provided, state) => {
+        const color = state.isFocused ? '#292626' : '#E5E5E5';
+        const backgroundColor = "#292626";
+        return { ...provided, color, backgroundColor };
+    }
+
+
+};
+
+function SelectFilter({ defaultValue, options, onChange, isMulti = false, label, tooltip, tooltipDisabled, onMenuOpen, onMenuClose, wide, parentRef }) {
+    return <ConditionalWrapper
+        condition={tooltip}
+        wrapper={
+            children => {
+                return <Tippy
+                    disabled={tooltipDisabled}
+                    placement='bottom'
+                    content={tooltip}
+                >
+                    {children}
+                </Tippy>;
+            }
+        }
+    >
+        <label
+            className={`single-filter-wrapper ${wide ? 'single-filter-wrapper-wide' : ''}`}
+        >
+            <span
+                className={'single-filter-label'}
+            >
+                {label}
+            </span>
+            <Select
+                className="basic-multi-select"
+                classNamePrefix="select"
+                defaultValue={defaultValue}
+                isMulti={isMulti}
+                name="colors"
+                onChange={onChange}
+                onMenuClose={onMenuClose}
+                onMenuOpen={onMenuOpen}
+                options={options}
+                ref={parentRef}
+                styles={selectFilterStyle}
+            />
+        </label>
+    </ConditionalWrapper>;
+};
+
+function InputFilter({ defaultValue, type, placeholder, onChange, label }) {
     return <label
-        className = {'single-filter-wrapper'}
+        className={'single-filter-wrapper'}
     >
         <span
-            className = {'single-filter-label'}
+            className={'single-filter-label'}
         >
             {label}
         </span>
         <input
-            className = 'filter-input'
-            defaultValue = {defaultValue}
-            type = {type}
-            placeholder = {placeholder}
-            onChange = {onChange}
+            className='filter-input'
+            defaultValue={defaultValue}
+            type={type}
+            placeholder={placeholder}
+            onChange={onChange}
         />
-    </label>
+    </label>;
 }
 
-function Filter ({center, children}) {
+function Filter({ center, children }) {
     const [showFilter, setShowFilter] = useState(false);
     return [
         <div
-            className = {`filter-toggle-icon-wrapper`}
-            key = 'filter-toggle-icon'
-            onClick = {e => setShowFilter(!showFilter)}
+            className={`filter-toggle-icon-wrapper`}
+            key='filter-toggle-icon'
+            onClick={e => setShowFilter(!showFilter)}
         >
             <FilterIcon />
         </div>,
         <div
-            className = {`filter-wrapper ${showFilter ? 'open': ''} ${center ? 'filter-wrapper-center': ''}`}
-            key = 'page-filter'
+            className={`filter-wrapper ${showFilter ? 'open' : ''} ${center ? 'filter-wrapper-center' : ''}`}
+            key='page-filter'
         >
             <div
-                className = {'filter-content-wrapper'}
+                className={'filter-content-wrapper'}
             >
                 {/* <div
                     className = {'text-label'}
                 >
                     {`Prices updated: ${new Date().toLocaleDateString()}`}
                 </div> */}
-                { children }
+                {children}
             </div>
         </div>
     ];
