ff9b036e37ba6cee54d956b4419060592116f23d
kokarn <kokarn@gmail.com>
2021-12-25 23:27:49 +0100

    add script to validate names

---
+++ scripts/test-redirects.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const got = require('got');
+
+const redirects = require('../workers-site/redirects.json');
+
+(async () => {
+    let liveNames = [];
+    try {
+        const response = await got.post('https://tarkov-tools.com/graphql', {
+            body: JSON.stringify({query: `{
+                itemsByType(type: any){
+                    normalizedName
+                }
+            }`
+            }),
+            responseType: 'json',
+        });
+
+        liveNames = response.body.data.itemsByType.map(item => item.normalizedName);
+    } catch (loadError){
+        console.error(loadError);
+
+        return false;
+    }
+
+    const keys = Object.keys(redirects);
+
+    for(const key of keys){
+        const itemName = key.replace('/item/', '');
+        if(!liveNames.includes(itemName)){
+            continue;
+        }
+
+        console.log(`${key} `);
+
+        Reflect.deleteProperty(redirects, key);
+    }
+
+    fs.writeFileSync(path.join(__dirname, '..', 'workers-site', 'redirects.json'), JSON.stringify(redirects, null, 4));
+})();
