a77e17e4f4bb074768ab89b60cf63fb378211f55
kokarn <kokarn@gmail.com>
2022-01-07 21:19:15 +0100

    remove unused code

--- src/pages/items/containers/index.js
+++ src/pages/items/containers/index.js
@@ -1,84 +1,9 @@
-import {useMemo} from 'react';
 import {Helmet} from 'react-helmet';
 
 import ID from '../../../components/ID.jsx';
-import ValueCell from '../../../components/value-cell';
 import SmallItemTable from '../../../components/small-item-table';
 
-const centerCell = ({ value }) => {
-    return <div
-        className = 'center-content'
-    >
-        { value }
-    </div>;
-};
-
 function Containers(props) {
-    const columns = useMemo(
-        () => [
-            {
-                accessor: 'image',
-                Cell: ({ value }) => {
-                    return <div
-                        className = 'center-content'
-                    >
-                        <img
-                            alt = ''
-                            className = 'table-image'
-                            height = '64'
-                            loading='lazy'
-                            src = { value }
-                            width = '64'
-                        />
-                    </div>
-                },
-            },
-            {
-                Header: 'Name',
-                accessor: 'name',
-                Cell: (cellData) => {
-                    const fullItemData = cellData.data.find(cellItem => cellItem.name === cellData.value);
-                    return <div
-                        className = 'center-content'
-                    >
-                        <a
-                            href = {fullItemData.itemLink}
-                        >
-                            {cellData.value}
-                        </a>
-                        {fullItemData.notes ? <cite>{fullItemData.notes}</cite> : ''}
-                    </div>
-                },
-            },
-            {
-                Header: 'Grid slots',
-                accessor: 'slots',
-                Cell: centerCell,
-            },
-            {
-                Header: 'Inner size',
-                accessor: 'size',
-                Cell: centerCell,
-            },
-            {
-                Header: 'Slot ratio',
-                accessor: 'ratio',
-                Cell: centerCell,
-            },
-            {
-                Header: 'Price',
-                accessor: 'price',
-                Cell: ValueCell,
-            },
-            {
-                Header: 'Price per slot',
-                accessor: 'pricePerSlot',
-                Cell: ValueCell,
-            },
-        ],
-        []
-    )
-
     return [<Helmet
         key = {'containers-table'}
     >
