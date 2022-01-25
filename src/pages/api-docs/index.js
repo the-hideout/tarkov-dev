6700d3e9cbcb5275f87deac3735a463530b38815
HeyBanditoz <banditoz@protonmail.com>
2022-01-25 01:48:41 -0700

    Fix Python example indentation and add import

--- src/pages/api-docs/index.js
+++ src/pages/api-docs/index.js
@@ -202,12 +202,14 @@ request('https://tarkov-tools.com/graphql', query).then((data) => console.log(da
                 language = 'python'
                 style = {monokai}
             >
-                {`def run_query(query):
-response = requests.post('https://tarkov-tools.com/graphql', json={'query': query})
-if response.status_code == 200:
-    return response.json()
-else:
-    raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
+                {`import requests
+
+def run_query(query):
+    response = requests.post('https://tarkov-tools.com/graphql', json={'query': query})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
 
 
 new_query = """
@@ -221,7 +223,7 @@ new_query = """
 """
 
 result = run_query(new_query)
-                print(result)`}
+print(result)`}
             </SyntaxHighlighter>
         </div>
         <div
