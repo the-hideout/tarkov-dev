d2794ce681ab327df7a7b9bb074ddf92390279ae
kokarn <kokarn@gmail.com>
2022-01-27 10:00:51 +0100

    Load contributors from all projects

--- scripts/get-contributors.js
+++ scripts/get-contributors.js
@@ -3,13 +3,37 @@ const path = require('path');
 
 const got = require('got');
 
+const repositories = [
+    'kokarn/tarkov-tools',
+    'kokarn/tarkov-tools-discord-bot',
+    'kokarn/tarkov-data-api',
+    'TarkovTracker/tarkovdata',
+];
+
 (async () => {
     console.log('Loading contributors');
-    console.time('contributors');
-    const data = await got('https://api.github.com/repos/kokarn/tarkov-tools/contributors', {
-        responseType: 'json',
-    });
-    console.timeEnd('contributors');
+    let allContributors = [];
 
-    fs.writeFileSync(path.join(__dirname, '..', 'src', 'data', 'contributors.json'), JSON.stringify(data.body, null, 4));
+    for(const repository of repositories){
+        console.time(`contributors-${repository}`);
+        const response = await got(`https://api.github.com/repos/${repository}/contributors`, {
+            responseType: 'json',
+        });
+        console.timeEnd(`contributors-${repository}`);
+
+        for(const contributor of response.body){
+            allContributors.push({
+                login: contributor.login,
+                html_url: contributor.html_url,
+            });
+        }
+    }
+
+    allContributors = allContributors.filter((value, index, self) => {
+        return index === self.findIndex((t) => (
+            t.place === value.place && t.login === value.login
+        ));
+    });
+
+    fs.writeFileSync(path.join(__dirname, '..', 'src', 'data', 'contributors.json'), JSON.stringify(allContributors, null, 4));
 })();
