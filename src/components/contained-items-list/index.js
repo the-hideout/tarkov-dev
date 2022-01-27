cb11f6bc6612b83d636fb92e6a1b37fba1ea68dd
kokarn <kokarn@gmail.com>
2022-01-27 15:48:00 +0100

    Sort the holdable items

--- src/components/contained-items-list/index.js
+++ src/components/contained-items-list/index.js
@@ -17,49 +17,78 @@ const ContainedItemsList = ({item}) => {
         return null;
     }
 
+    const sortedItems = [...item.canHoldItems];
+
+    sortedItems.sort((a, b) => {
+        const firstCategory = categoryData[a];
+        const secondCategory = categoryData[b];
+
+        let firstTitle = firstCategory?.urlName;
+        let secondTitle = secondCategory?.urlName;
+
+        if(!firstCategory){
+            const firstItem = items.find(item => item.id === a);
+
+            firstTitle = firstItem.normalizedName;
+        }
+
+        if(!secondCategory){
+            const secondItem = items.find(item => item.id === b);
+
+            secondTitle = secondItem.normalizedName;
+        }
+
+        if(!firstTitle || !secondTitle){
+            return 0;
+        }
+
+        return firstTitle.localeCompare(secondTitle);
+    });
+
     return <div>
         <span
             className='contained-item-title-wrapper'
         >
             Can hold:
         </span>
-        {item.canHoldItems.map((itemOrCategoryId, index) => {
-            const currentCategoryData = categoryData[itemOrCategoryId];
-            let linkedItem = false;
+        {sortedItems.map((itemOrCategoryId, index) => {
+                const currentCategoryData = categoryData[itemOrCategoryId];
+                let linkedItem = false;
 
-            if(itemOrCategoryId === '54009119af1c881c07000029'){
-                // Special case for items that can contain all items
+                if(itemOrCategoryId === '54009119af1c881c07000029'){
+                    // Special case for items that can contain all items
 
-                return null;
-            }
+                    return null;
+                }
 
-            if(!currentCategoryData){
-                linkedItem = items.find(item => item.id === itemOrCategoryId);
-            }
+                if(!currentCategoryData){
+                    linkedItem = items.find(item => item.id === itemOrCategoryId);
+                }
 
-            if(!linkedItem && !currentCategoryData){
-                console.log(itemOrCategoryId);
+                if(!linkedItem && !currentCategoryData){
+                    console.log(itemOrCategoryId);
 
-                return null;
-            }
+                    return null;
+                }
 
-            if(linkedItem && linkedItem.types.includes('disabled')){
-                return null;
-            }
+                if(linkedItem && linkedItem.types.includes('disabled')){
+                    return null;
+                }
 
-            return <span
-                className='contained-item-link-wrapper'
-                key = {`item-link-${item.id}-${linkedItem?.id || currentCategoryData._id}`}
-            >
-                <Link
-                    className="contained-item-link"
-                    to={currentCategoryData ? `/items/${currentCategoryData?.urlName}` : `/item/${linkedItem.normalizedName}`}
+                return <span
+                    className='contained-item-link-wrapper'
+                    key = {`item-link-${item.id}-${linkedItem?.id || currentCategoryData._id}`}
                 >
-                    {currentCategoryData ? formatCategoryName(currentCategoryData) : linkedItem.name}
-                </Link>
-                {item.canHoldItems.length > index + 1 ? ',' : ''}
-            </span>
-        })}
+                    <Link
+                        className="contained-item-link"
+                        to={currentCategoryData ? `/items/${currentCategoryData?.urlName}` : `/item/${linkedItem.normalizedName}`}
+                    >
+                        {currentCategoryData ? formatCategoryName(currentCategoryData) : linkedItem.name}
+                    </Link>
+                    {item.canHoldItems.length > index + 1 ? ',' : ''}
+                </span>
+            }
+        )}
     </div>
 };
 
