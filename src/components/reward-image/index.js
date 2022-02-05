ded5b7b522a9938099ea74379097d8d26c31db2f
ATunaMelt <ATunaMelt@protonmail.com>
2022-02-05 17:12:40 -0500

    refactor: Improved visibility of the number of items required for crafts/barters

--- src/components/reward-image/index.js
+++ src/components/reward-image/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-function RewardImage({ count, iconLink }) {
+function RewardImage({ count, iconLink, height = '64', width = '64' }) {
     return (
         <div className="reward-image-wrapper">
             {count && <span className="reward-count-wrapper">{count}</span>}
@@ -8,8 +8,8 @@ function RewardImage({ count, iconLink }) {
                 alt=""
                 className="table-image"
                 loading="lazy"
-                height="64"
-                width="64"
+                height={height}
+                width={width}
                 src={iconLink}
             />
         </div>
