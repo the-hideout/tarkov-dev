df78323883599892a9f2ff8443cf3c921ea3ee2d
kokarn <kokarn@gmail.com>
2022-02-21 15:16:54 +0100

    Don't do anything with the serviceWorker for now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import store from './store';
@@ -39,4 +39,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
