4171585f12e4bbc0d1efd6ac137e608c80398f79
kokarn <kokarn@gmail.com>
2022-03-02 16:25:26 +0100

    basic trader filter

--- src/pages/gun-builder/index.js
+++ src/pages/gun-builder/index.js
@@ -1,9 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import TarkovGunBuilder from 'tarkov-gun-builder';
 
 import { useItemsQuery } from '../../features/items/queries';
+import {
+    Filter,
+    ToggleFilter
+} from '../../components/filter';
+import useStateWithLocalStorage from '../../hooks/useStateWithLocalStorage';
 
 import './index.css';
 
@@ -12,6 +17,7 @@ function GunBuilder() {
     const [gamePresets, setGamePresets] = useState({});
     const [defaultPresets, setDefaultPresets] = useState([]);
     const { data: items } = useItemsQuery();
+    const [showOnlyTrader, setShowOnlyTrader] = useStateWithLocalStorage('showOnlyTraderModsInBuilder', false);
 
     useEffect(() => {
         fetch(`${process.env.PUBLIC_URL}/data/globals.min.json`)
@@ -29,6 +35,33 @@ function GunBuilder() {
             });
     }, []);
 
+    const availableItemIds = useMemo(() => {
+        return items
+            .filter((item) => {
+                return item.buyFor.length > 0;
+            })
+            .filter((item) => {
+                if(!showOnlyTrader){
+                    return true;
+                }
+
+                // At least one trader price
+                if(item.buyFor.length > 1){
+                    return true;
+                }
+
+                // One price that's not from flea market
+                if(item.buyFor[0].source !== 'flea-market'){
+                    return true;
+                }
+
+                return false;
+            })
+            .map((item) => {
+                return item.id;
+            });
+    }, [items, showOnlyTrader]);
+
     return [
         <Helmet key={'gun-builder-helmet'}>
             <meta charSet="utf-8" />
@@ -41,8 +74,23 @@ function GunBuilder() {
         <div className="page-wrapper" key={'display-wrapper'}>
             <div className="page-headline-wrapper">
                 <h1>{t('Escape from Tarkov Gun Builder')}</h1>
+                <Filter>
+                    <ToggleFilter
+                        checked={showOnlyTrader}
+                        label={t('Show only items available from traders')}
+                        onChange={(e) => setShowOnlyTrader(!showOnlyTrader)}
+                        // tooltipContent={
+                        //     <div>
+                        //         {t(
+                        //             'Dogtags are very hard to calulcate values for as they depend on level. Most calculations with them are wrong',
+                        //         )}
+                        //     </div>
+                        // }
+                    />
+                </Filter>
             </div>
             <TarkovGunBuilder
+                avialableItemIds={availableItemIds}
                 items={items}
                 presets={gamePresets.ItemPresets}
                 defaultPresets={defaultPresets}
