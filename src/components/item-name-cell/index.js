92a75a41ec4a9aceca4a96fe3223c4782d8c2dc7
austinhodak <ahodak65@gmail.com>
2022-03-26 00:05:28 -0400

    Let's make the icon clickable also, not just the text.

--- src/components/item-name-cell/index.js
+++ src/components/item-name-cell/index.js
@@ -8,14 +8,18 @@ function ItemNameCell(props) {
     return (
         <div className="small-item-table-description-wrapper">
             <div className="small-item-table-image-wrapper">
-                <img
-                    alt=""
-                    className="table-image"
-                    height="64"
-                    loading="lazy"
-                    src={props.row.original.iconLink}
-                    width="64"
-                />
+                 <Link
+                     to={props.row.original.itemLink}
+                     className="small-item-table-image-link">
+                     <img
+                         alt=""
+                         className="table-image"
+                         height="64"
+                         loading="lazy"
+                         src={props.row.original.iconLink}
+                         width="64"
+                     />
+                 </Link>
             </div>
             <div className="small-item-table-name-wrapper">
                 <Link
