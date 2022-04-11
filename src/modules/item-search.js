b80a1e48f9a51d6fda78d8bfd72fbba163770f59
Grant Birkinbine <grantbirki@github.com>
2022-04-11 13:30:57 -0700

    remove old comments

--- src/modules/item-search.js
+++ src/modules/item-search.js
@@ -18,24 +18,19 @@ const itemSearch = (items, searchString) => {
         (item) => formatName(item.shortName) === formattedSearchString,
     );
 
-    // console.log(`Length after shortname ${matches.length}`);
-
     if (!matches || matches.length === 0) {
-        // console.log('Checking for name match');
         matches = items.filter((item) =>
             formatName(item.name).includes(formattedSearchString),
         );
     }
 
     if (!matches || matches.length === 0) {
-        // console.log('Checking for shortName match');
         matches = items.filter((item) =>
             formatName(item.shortName).includes(formattedSearchString),
         );
     }
 
     if (!matches || matches.length === 0) {
-        // console.log('Doing fuse search');
         const options = {
             includeScore: true,
             keys: ['name'],
@@ -44,8 +39,6 @@ const itemSearch = (items, searchString) => {
 
         const fuse = new Fuse(items, options);
         const result = fuse.search(formattedSearchString);
-
-        // console.log(result);
 
         matches = result.map((resultObject) => resultObject.item);
     }
