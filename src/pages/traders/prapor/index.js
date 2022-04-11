122116bb540ecb3b05f9ebfe0dd8fe74e5263839
Grant Birkinbine <grantbirki@github.com>
2022-04-11 13:19:21 -0700

    update all individual trader descriptions for their pages

--- src/pages/traders/prapor/index.js
+++ src/pages/traders/prapor/index.js
@@ -45,7 +45,7 @@ function Prapor() {
             <title>{t('Prapor Items')}</title>
             <meta
                 name="description"
-                content={`All the relevant information about Escape from Tarkov`}
+                content={`All Prapor items and barters in Escape from Tarkov`}
             />
         </Helmet>,
         <div className="page-wrapper" key={'display-wrapper'}>
