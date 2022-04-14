5bee79cc1ccc02618b017f3c1efac4579ed27e42
Grant Birkinbine <grantbirki@github.com>
2022-04-13 22:16:10 -0400

    remove tarkov gun builder due to vuln in dependency

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ import ItemTracker from './pages/item-tracker/';
 import Hideout from './pages/hideout';
 import WipeLength from './pages/wipe-length';
 import About from './pages/about/';
-import GunBuilder from './pages/gun-builder';
+// import GunBuilder from './pages/gun-builder';
 
 import Guides from './pages/guides';
 
@@ -692,7 +692,7 @@ function App() {
                         remoteControlSessionElement,
                     ]}
                 />
-                <Route
+                {/* <Route
                     path="/gun-builder"
                     element={[
                         <div
@@ -703,7 +703,7 @@ function App() {
                         </div>,
                         remoteControlSessionElement,
                     ]}
-                />
+                /> */}
                 <Route
                     path="*"
                     element={[<ErrorPage />, remoteControlSessionElement]}
