66058c617daaae7f0eff9cf2d32a30f43d6f4097
Anton Shebukov <shebuka@hotmail.com>
2022-06-21 15:17:25 +0300

    Added isTool parameter to reward image builder

--- src/components/reward-image/index.js
+++ src/components/reward-image/index.js
@@ -1,12 +1,12 @@
 import './index.css';
 
-function RewardImage({ count, iconLink, height = '64', width = '64' }) {
+function RewardImage({ count, iconLink, height = '64', width = '64', isTool = false }) {
     return (
         <div className="reward-image-wrapper">
             {count && <span className="reward-count-wrapper">{count}</span>}
             <img
                 alt=""
-                className="table-image"
+                className={isTool === false ? "table-image" : "table-image-tool"}
                 loading="lazy"
                 height={height}
                 width={width}
