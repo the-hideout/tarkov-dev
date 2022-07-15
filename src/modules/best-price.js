5c458f41c291ad714d7def17b6a01497d31392f8
Razzmatazz <razz@badkarma.xyz>
2022-07-15 08:43:27 -0500

    use lastLowPrice for best price

--- src/modules/best-price.js
+++ src/modules/best-price.js
@@ -1,12 +1,12 @@
 const calculateFee = require('./flea-market-fee');
 
 module.exports = (itemData) => {
-    let currentFee = calculateFee(itemData.basePrice, itemData.avg24hPrice);
-    let bestProfit = itemData.avg24hPrice - currentFee;
-    let bestPrice = itemData.avg24hPrice;
+    let currentFee = calculateFee(itemData.basePrice, itemData.lastLowPrice);
+    let bestProfit = itemData.lastLowPrice - currentFee;
+    let bestPrice = itemData.lastLowPrice;
     let bestPriceFee = currentFee;
 
-    for (let i = itemData.avg24hPrice - 1000; i > 0; i = i - 1000) {
+    for (let i = itemData.lastLowPrice - 1000; i > 0; i = i - 1000) {
         const newFee = calculateFee(itemData.basePrice, i);
 
         const newProfit = i - newFee;
