e8fe0eb73a79738afa2c625e6ba7d98457f16683
SpitefulCartographer <blakebenoit@protonmail.com>
2022-08-02 03:55:47 -0500

    Revert "Merge branch 'main' of https://github.com/SpitefulCartographer/tarkov-dev"
    This reverts commit ca2153b91a29165f210af1b391b5ad21b25e3007, reversing
    changes made to 1bc3a3c5fe90447f9c0764d79e2df60a39e64304.

--- src/components/small-item-table/index.js
+++ src/components/small-item-table/index.js
@@ -322,8 +322,7 @@ function SmallItemTable(props) {
                         (barter) => barter.rewardItems[0].item.id === itemData.id,
                     ),
                     grid: itemData.grid,
-                    pricePerSlot: itemData.itemProperties.grid?.totalSize - itemData.slots !==0 ? // Prevents divide by zero errors.
-                                  Math.floor(itemData.avg24hPrice / (itemData.itemProperties.grid?.totalSize - itemData.slots)) : 0,
+                    pricePerSlot: Math.floor(itemData.avg24hPrice / (itemData.itemProperties.grid?.totalSize - itemData.slots)),
                     ratio: (itemData.itemProperties.grid?.totalSize / itemData.slots).toFixed(2),
                     size: itemData.itemProperties.grid?.totalSize,
                     notes: itemData.notes,
@@ -346,8 +345,7 @@ function SmallItemTable(props) {
                     formattedItem.barterPrice = getCheapestBarter(itemData, formattedItem.barters);
 
                     if (!itemData.avg24hPrice || formattedItem.barterPrice.price < itemData.avg24hPrice) {
-                        formattedItem.pricePerSlot =  itemData.itemProperties.grid?.totalSize - itemData.slots ? // Prevents divide by zero errors.
-                                                      Math.floor(formattedItem.barterPrice.price / (itemData.itemProperties.grid?.totalSize - itemData.slots)) : 0;
+                        formattedItem.pricePerSlot = Math.floor(formattedItem.barterPrice.price / (itemData.itemProperties.grid?.totalSize - itemData.slots));
                     }
                 }
 
