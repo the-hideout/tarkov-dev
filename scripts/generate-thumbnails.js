572cb1bac67230c674d18bea584ce04cd012bd4b
Razzmatazz <razz@badkarma.xyz>
2022-08-02 17:18:51 -0500

    automate Shebuka's map thumbnails

---
+++ scripts/generate-thumbnails.js
@@ -0,0 +1,21 @@
+const fs = require('fs/promises');
+
+const Jimp = require('jimp');
+
+(async () => {
+    console.time('Generating thumbnails');
+    
+    // Max height from css ".map-wrapper img"
+    const maxHeight = 200;
+    const mapsPath = './public/maps/';
+    const files = await fs.readdir(mapsPath);
+    for (const fileName of files) {
+        if (!fileName.endsWith('.jpg')) continue;
+        if (fileName.endsWith('_thumb.jpg')) continue;
+        const thumbName = fileName.replace('.jpg', '_thumb.jpg');
+        console.log(`Generating ${thumbName}`);
+        const image = (await Jimp.read(mapsPath+fileName)).resize(Jimp.AUTO, maxHeight).quality(70);
+        image.write(mapsPath+thumbName);
+    }
+    console.timeEnd('Generating thumbnails');
+})();
