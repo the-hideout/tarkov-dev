ebf773dfc0962da869a387453f2ae97f98769161
Grant Birkinbine <grantbirki@github.com>
2022-08-03 10:23:13 -0600

    add critical

---
+++ scripts/critical.js
@@ -0,0 +1,15 @@
+const critical = require('critical');
+
+critical.generate({
+    base: 'build/',
+    src: './index.html',
+    inline: true,
+    css: ['build/static/css/*.css'],
+    target: 'index.html'
+  }, (err, output) => {
+    if (err) {
+      console.error(err);
+    } else if (output) {
+      console.log('Generated critical CSS');
+    }
+  });
