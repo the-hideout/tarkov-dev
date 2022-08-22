cf027b68f6ad75a03dc09b7e04da672d0663fd79
Grant Birkinbine <grantbirki@github.com>
2022-08-22 10:03:27 -0600

    working simple boss page

--- src/pages/bosses/boss/index.js
+++ src/pages/bosses/boss/index.js
@@ -1,62 +1,89 @@
 import { Helmet } from 'react-helmet';
 import React, { Suspense } from 'react';
-import Icon from '@mdi/react';
-import { mdiEmoticonDevil } from '@mdi/js';
 import { useTranslation } from 'react-i18next';
-import { useParams, Navigate, Link } from 'react-router-dom';
-
-import { BossPageList } from '../../../components/boss-list';
+import { useParams } from 'react-router-dom';
+import capitalize from '../../../modules/capitalize-first';
+import formatBossData from '../../../modules/format-boss-data';
+import { useBossesQuery } from '../../../components/boss-list';
 
 import './index.css';
 
 const renderLoader = () => <p>Loading...</p>;
 
-function Boss(props) {
+function BossPage(bossName) {
+    const bossNameLower = bossName.bossName
     const { t } = useTranslation();
+
+    // Fetch bosses
+    const { data: bosses } = useBossesQuery();
+
+    // If no bosses have been returned yet, return 'loading'
+    if (!bosses || bosses.length === 0) {
+        return 'Loading...';
+    }
+
+    // Format the boss data
+    const bossArray = formatBossData(bosses);
+
+    var bossData;
+    for (const boss of bossArray) {
+        if (boss.name.toLowerCase() === bossNameLower) {
+            bossData = boss;
+            break;
+        }
+    }
+
+    return [
+        <div className="display-wrapper" key={'display-wrapper'}>
+            <div className={'item-page-wrapper'}>
+                <div className="main-information-grid">
+                    <div className="item-information-wrapper">
+                        <h1>
+                            <div className={'item-font'}>
+                                {bossData.name}
+                            </div>
+                        </h1>
+                        <span className="wiki-link-wrapper">
+                            <a href={`https://escapefromtarkov.fandom.com/wiki/${bossData.name}`}>
+                                {t('Wiki')}
+                            </a>
+                        </span>
+                    </div>
+                    <div className="icon-and-link-wrapper">
+                        <img
+                            alt={bossData.name}
+                            className={'item-image'}
+                            loading="lazy"
+                            src={`https://assets.tarkov.dev/${bossNameLower}.jpg`}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    ]
+}
+
+function Boss() {
     const { bossName } = useParams();
+    const boss = capitalize(bossName);
 
-    return (<p>{bossName}</p>);
-
-    // return [
-    //     <Helmet key={'loot-tier-helmet'}>
-    //         <meta charSet="utf-8" />
-    //         <title>Escape from Tarkov Bosses</title>
-    //         <meta
-    //             name="description"
-    //             content="Escape from Tarkov Bosses, boss details, and more"
-    //         />
-    //     </Helmet>,
-    //     <div className={'page-wrapper'} key="boss-page-wrapper">
-    //         <h1 className="center-title">
-    //             <Icon
-    //                 path={mdiEmoticonDevil}
-    //                 size={1.5}
-    //                 className="icon-with-text"
-    //             />
-    //             {t('Bosses')}
-    //         </h1>
-    //         <h2 className='boss-sub-text'>Bosses are feared and deadly enemies with unique gear and traits in Escape from Tarkov</h2>
-    //         <div className="boss-list-wrapper">
-    //             <Suspense fallback={renderLoader()}>
-    //                 <BossPageList />
-    //             </Suspense>
-    //         </div>
-
-    //         <hr className='desc-line-break'></hr>
-
-    //         <div className="page-wrapper boss-page-wrapper">
-    //             <h3>About Bosses</h3>
-    //             <p>
-    //                 {"In Escape from Tarkov, there are many bosses that roam the area of besieged Norvinsk. "}<br />
-    //                 <br />
-    //                 {"Each boss has unique behaviors, characteristics, and tactics. The bosses in Tarkov are feared by players of all levels and will often pose as a greater threat than enemy PMCs in the region."}<br />
-    //                 <br />
-    //                 {"However, with high risk comes high reward. Many bosses contain high tier loot items or are required to elimate for quests."}
-    //                 {"Learning the patterns, locations, and distinct attire of a boss is often the best a player can prepare themselves when a fight begins against a boss in Tarkov."}<br />
-    //             </p>
-    //         </div>
-    //     </div>,
-    // ];
+    return [
+        <Helmet key={'loot-tier-helmet'}>
+            <meta charSet="utf-8" />
+            <title>{`${boss} - Escape from Tarkov`}</title>
+            <meta
+                name="description"
+                content={`All the relevant information about ${boss} (boss) in Escape from Tarkov`}
+            />
+            <link
+                rel="canonical"
+                href={`https://tarkov.dev/boss/${bossName}`}
+            />
+        </Helmet>,
+        <Suspense fallback={renderLoader()}>
+            <BossPage bossName={bossName} />
+        </Suspense>
+    ];
 }
 
 export default Boss;
