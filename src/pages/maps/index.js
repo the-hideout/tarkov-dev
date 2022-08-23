c905d44823411b62404f461a7e815f1fa8f38373
Shebuka <shebuka@hotmail.com>
2022-08-23 14:43:06 +0200

    Using maps info and grouping

--- src/pages/maps/index.js
+++ src/pages/maps/index.js
@@ -25,25 +25,35 @@ function Maps() {
                 <Icon path={mdiMap} size={1.5} className="icon-with-text" /> 
                 {t('Maps')}
             </h1>
-            <div className="maps-wrapper">
-                {rawMapData.map((map) => {
-                    const { displayText, key } = map;
-                    return (
-                        <div className="map-wrapper" key={`map-wrapper-${key}`}>
-                            <h2>{displayText}</h2>
-                            <Link to={`/map/${key}`}>
-                                <img
-                                    alt={`Map of ${displayText}`}
-                                    className="map-image"
-                                    loading="lazy"
-                                    title={`Map of ${displayText}`}
-                                    src={`${process.env.PUBLIC_URL}/maps/${key}_thumb.jpg`}
-                                />
-                            </Link>
+            {rawMapData.map((mapsGroup) => {
+                return (
+                    <>
+                        <h2>{mapsGroup.name}</h2>
+                        <div className="page-wrapper map-page-wrapper">
+                            {mapsGroup.description}
                         </div>
-                    );
-                })}
-            </div>
+                        <div className="maps-wrapper">
+                        {mapsGroup.maps.map((amap) => {
+                            const { displayText, key } = amap;
+                            return (
+                                <div className="map-wrapper" key={`map-wrapper-${key}`}>
+                                    <h3>{displayText}</h3>
+                                    <Link to={`/map/${key}`}>
+                                        <img
+                                            alt={`Map of ${displayText}`}
+                                            className="map-image"
+                                            loading="lazy"
+                                            title={`Map of ${displayText}`}
+                                            src={`${process.env.PUBLIC_URL}/maps/${key}_thumb.jpg`}
+                                        />
+                                    </Link>
+                                </div>
+                            );
+                        })}
+                        </div>
+                    </>
+                );
+            })}
             <div className="page-wrapper map-page-wrapper">
                 <p>
                     {"There are 12 different locations on the Escape from Tarkov map, of which 8 have been released publicly so far. Although eventually all maps will be connected, they are currently all apart from one another."}
