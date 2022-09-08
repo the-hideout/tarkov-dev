4e3200229d9f7f98bbbe1d0bb968b70d2ddacf6f
Grant Birkinbine <grantbirki@github.com>
2022-09-07 21:29:48 -0600

    add contributors script to get an quick Avatar component

---
+++ src/components/contributors/index.js
@@ -0,0 +1,46 @@
+import { AvatarStack, Avatar } from '@primer/react';
+
+import contributorJson from '../../data/contributors.json';
+
+// inputs
+// size: number (pixels) of the size of the avatar
+// quality: number (pixels) of the quality of the avatar
+// stack: boolean (true/false) to stack the avatars
+function Contributors(props) {
+    var quality;
+    if (!props.quality) {
+        quality = props.size;
+    } else {
+        quality = props.quality;
+    }
+
+    if (props.stack === true) {
+        return (
+            <AvatarStack>
+                {contributorJson.map((contributor) => (
+                    <Avatar
+                        key={contributor.login}
+                        src={`${contributor.avatar_url}&size=${quality}`}
+                        alt={contributor.login}
+                        size={props.size}
+                    ></Avatar>
+                ))}
+            </AvatarStack>
+        );
+    } else {
+        return (
+            <>
+                {contributorJson.map((contributor) => (
+                    <a href={contributor.html_url}>
+                        <Avatar
+                            size={props.size}
+                            src={`${contributor.avatar_url}&size=${quality}`}
+                        />
+                    </a>
+                ))}
+            </>
+        );
+    }
+}
+
+export default Contributors;
