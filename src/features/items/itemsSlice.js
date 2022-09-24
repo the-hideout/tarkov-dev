c8c009c89027610107ae830bcaf6c8f21553c012
Razzmatazz <razz@badkarma.xyz>
2022-09-24 17:49:35 -0500

    fix ci import error

--- src/features/items/itemsSlice.js
+++ src/features/items/itemsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import equal from 'fast-deep-equal';
 
-import doFetchItems from './do-fetch-items';
+import doFetchItems from './do-fetch-items.mjs';
 
 const initialState = {
     items: [],
