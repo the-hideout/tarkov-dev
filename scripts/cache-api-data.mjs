7797c4899835616efb82d54f9ed8db38800a6647
Razzmatazz <razz@badkarma.xyz>
2022-09-25 07:39:52 -0500

    more streamlinging

--- scripts/cache-api-data.mjs
+++ scripts/cache-api-data.mjs
@@ -5,7 +5,6 @@ import doFetchItems from '../src/features/items/do-fetch-items.mjs';
 console.time('Caching API data');
 try {
     const items = await doFetchItems('en', true);
-    console.log(items.length);
     await fs.writeFile('./src/data/items.json', JSON.stringify(items));
 } catch (error) {
     console.log(error);
