dba470df7bef282294ef06b1b2d7f776176d4e76
Shebuka <shebuka@hotmail.com>
2023-03-02 15:35:01 +0100

    Added FIR icon to craft rewards

--- src/components/reward-image/index.js
+++ src/components/reward-image/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-function RewardImage({ count, iconLink, height = '64', width = '64', isTool = false, nonFunctional = false }) {
+function RewardImage({ count, iconLink, height = '64', width = '64', isTool = false, nonFunctional = false, isFIR = false }) {
     let imageClass = 'reward-image-img';
     if (isTool) {
         imageClass = 'reward-image-img-tool';
@@ -9,7 +9,10 @@ function RewardImage({ count, iconLink, height = '64', width = '64', isTool = fa
     }
     return (
         <div className="reward-image-wrapper">
-            {count && <span className="reward-image-count">{count}</span>}
+            <div className="reward-image-extra-wrapper">
+                {isFIR && <img alt="" className="reward-image-fir" loading="lazy" src={`${process.env.PUBLIC_URL}/images/icon-fir.png`} />}
+                {count && <span className="reward-image-count">{count}</span>}
+            </div>
             <img
                 alt=""
                 className={imageClass}
