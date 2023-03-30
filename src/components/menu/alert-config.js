3ccc2518bdaeff3ddd7886a6ed928a2e885ccd02
grantbirki <grantbirki@github.com>
2023-03-30 22:38:25 +0100

    completely rework the way the alert banner operates

---
+++ src/components/menu/alert-config.js
@@ -0,0 +1,17 @@
+const alertConfig = {
+    // set this bool if the site alert should be enabled or not
+    alertEnabled: true,
+
+    // valid alert colors
+    alertColors: {
+        error: '#D3302F',
+        info: '#0088D1',
+        success: '#378E3C',
+        warning: '#F57D01',
+    },
+
+    // set this variable to the severity of the alert banner
+    alertLevel: 'info'
+}
+
+export default alertConfig
