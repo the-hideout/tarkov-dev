8b8710293b3a88f8de688f531f20352073e3ee16
Razzmatazz <razz@badkarma.xyz>
2023-04-12 18:22:47 -0500

    resolve conflicts

--- src/components/crafts-table/index.js
+++ src/components/crafts-table/index.js
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
