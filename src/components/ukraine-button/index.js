6ba6503736328e1c0e25c1f2b983424d284c278c
Shebuka <shebuka@hotmail.com>
2023-04-20 10:24:07 +0200

    Cleanup buttons style

--- src/components/ukraine-button/index.js
+++ src/components/ukraine-button/index.js
@@ -2,12 +2,12 @@ import { useTranslation } from 'react-i18next';
 import Button from '@mui/material/Button';
 import './index.css';
 
-function UkraineButton({ onlyLarge, linkStyle, wrapperStyle, text, children }) {
+function UkraineButton({ large=false, linkStyle }) {
     const { t } = useTranslation();
 
     return (
         <Button
-            style={linkStyle}
+            style={{...linkStyle, ...(large ? {width: "100%"} : '')}}
             className={'ua-button'}
             variant="contained"
             href="https://www.icrc.org/en/donate/ukraine"
@@ -16,17 +16,6 @@ function UkraineButton({ onlyLarge, linkStyle, wrapperStyle, text, children }) {
             {t('Support Ukraine')}
         </Button>
     );
-
-    // return (
-    //     <p
-    //         className={`ukraine-wrapper ${onlyLarge ? 'only-large' : ''}`}
-    //         style={wrapperStyle}
-    //     >
-    //         <a href="https://www.icrc.org/en/donate/ukraine" style={linkStyle} target="_blank" rel="noopener noreferrer">
-    //             {children ? children : t('Support Ukraine')}
-    //         </a>
-    //     </p>
-    // );
 }
 
 export default UkraineButton;
