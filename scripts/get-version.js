fd954a80bf4ea503c3513442d8466186c3f4069e
Shebuka <shebuka@hotmail.com>
2023-06-19 11:14:15 +0200

    Refactoring getVersion

--- scripts/get-version.js
+++ scripts/get-version.js
@@ -1,77 +1,65 @@
 const fs = require('fs');
 const path = require('path');
-const got = require('got');
 
 const fetch = require('cross-fetch');
 
 const COMMIT_URL = 'https://api.github.com/repos/the-hideout/tarkov-dev/commits/main';
 
-// Use a GitHub token to increase the rate limit
+// If a GitHub token is provided, use it to increase the rate limit
 const token = process.env.GITHUB_TOKEN;
 const headers = {};
 if (token) {
     headers.authorization = `token ${token}`;
-    console.log("Using provided GitHub token to increase rate limit")
+    console.log("Using provided GitHub token to increase rate limit");
 } else {
-    console.log("No GitHub token provided, rate limit may be reached")
-    console.warn("To increase the rate limit, provide a GitHub token via the GITHUB_TOKEN environment variable")
+    console.log("No GitHub token provided, rate limit may be reached");
+    console.warn("To increase the rate limit, provide a GitHub token via the GITHUB_TOKEN environment variable");
 }
 
-const getVersion = async function getVersion() {
+async function getVersion() {
     console.time(`Get version url ${COMMIT_URL}`);
+    let responseJson = {};
+
     try {
         const response = await fetch(COMMIT_URL, {
             headers,
             signal: AbortSignal.timeout(5000),
-        }).then(response => response.json());
-        console.timeEnd(`Get version url ${COMMIT_URL}`);
+        });
 
-        return response;
-    } catch (responseError) {
-        console.timeEnd(`Get version url ${COMMIT_URL}`);
-
-        if (responseError instanceof got.HTTPError) {
-            console.error(`HTTP Error: ${responseError.response.statusCode} ${responseError.response.statusMessage}`);
-        } else {
-            console.error(responseError);
+        if (!response.ok) {
+            console.error(`Error! status: ${response.status} message: ${response.statusText}`);
         }
+        else {
+            responseJson = await response.json();
+        }
+    } catch (responseError) {
+        console.error(`Error: ${responseError.message}`);
     }
 
-    return false;
-};
+    console.timeEnd(`Get version url ${COMMIT_URL}`);
+
+    return responseJson;
+}
 
 (async () => {
-    try {
-        let response = false;
+        const responseJson = await getVersion();
+        let versionDic = {};
 
-        response = await getVersion();
-
-        if (!response) {
-            console.log('Error fetching version, using fallback version.json')
-            fs.writeFileSync(path.join(__dirname, '..', 'src', 'data', 'version.json'), JSON.stringify(
-                {
-                    version: 'unknown'
-                }, null, 4
-            ));
-            return;
+        if (!responseJson || !responseJson.sha) {
+            console.log('Error fetching version, using fallback version.json (offline mode?)');
+            
+            versionDic = { version: 'unknown' };
+        }
+        else {
+            versionDic = { version: responseJson.sha };
         }
 
-        console.log(response.sha);
-
-        const version = {
-            version: response.sha || 'unknown',
-        }
+        console.log(versionDic);
 
         console.time('Write new data');
-        fs.writeFileSync(path.join(__dirname, '..', 'src', 'data', 'version.json'), JSON.stringify(version, null, 4));
+
+        let stringifyed = JSON.stringify(versionDic, null, 4);
+        fs.writeFileSync(path.join(__dirname, '..', 'src', 'data', 'version.json'), stringifyed);
+
         console.timeEnd('Write new data');
-    } catch (error) {
-        console.error(error);
-        console.log('Error fetching version, using fallback version.json')
-        fs.writeFileSync(path.join(__dirname, '..', 'src', 'data', 'version.json'), JSON.stringify(
-            {
-                version: 'unknown'
-            }, null, 4
-        ));
-    }
 })()
