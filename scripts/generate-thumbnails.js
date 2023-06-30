d3c4197c706f279136e6c7298d211a3407d36c5a
Razzmatazz <razz@badkarma.xyz>
2023-06-30 07:46:00 -0500

    generate thumbnails

--- scripts/generate-thumbnails.js
+++ scripts/generate-thumbnails.js
@@ -17,5 +17,20 @@ const sharp = require('sharp');
         const image = sharp(mapsPath+fileName).resize(null, maxHeight).jpeg({mozjpeg: true, quality: 90});
         await image.toFile(mapsPath+thumbName);
     }
+    const mapGroups = JSON.parse(await fs.readFile('./src/data/maps.json'));
+    for (const group of mapGroups) {
+        for (const map of group.maps) {
+            if (map.projection !== 'interactive') {
+                continue;
+            }
+            let path = map.mapPath || `https://assets.tarkov.dev/maps/${group.normalizedName}/{z}/{x}/{y}.png`;
+            path = path.replace(/{[xyz]}/g, '0');
+            const imageRequest = await fetch(path);
+            const image = sharp(await imageRequest.arrayBuffer()).trim('#00000000').resize(null, maxHeight).jpeg({mozjpeg: true, quality: 90});
+            const thumbName = `${group.normalizedName}_thumb.jpg`;
+            console.log(`Generating ${thumbName}`);
+            await image.toFile(mapsPath+thumbName);
+        }
+    }
     console.timeEnd('Generating thumbnails');
 })();
