385d1d63ba660a5a19fdf7beb2e6a18f0369c53b
Razzmatazz <razz@badkarma.xyz>
2023-07-03 16:51:29 -0500

    tweak default zooms

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -311,7 +311,7 @@ function Map() {
         }
 
         // Set default zoom level
-        map.fitWorld({maxZoom: mapData.defaultZoom || 3});
+        map.fitWorld({maxZoom: Math.max(mapData.maxZoom-3, mapData.minZoom)});
 
         mapRef.current = map;
     }, [mapData, mapRef, t]);
