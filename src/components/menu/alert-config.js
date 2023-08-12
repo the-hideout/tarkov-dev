8d190ecba1e2aa4ea63e4f49b7a63bde69972b61
Thaddeus Bond <thaddeus@thaddeus.io>
2023-08-12 03:29:10 +0000

    Update scanner status alert

--- src/components/menu/alert-config.js
+++ src/components/menu/alert-config.js
@@ -14,7 +14,7 @@ const alertConfig = {
     alertLevel: 'info',
 
     // The text to display in the alert banner
-    text: 'We are currently leveling our scanners for patch 0.13.5 - If you enjoy using tarkov.dev, please consider donating to help keep it running. All donations go directly towards server costs and operational expenses. This banner will only be up for a week. Thank you! ❤️',
+    text: '🌟 Flea market scanners have been leveled and flea market prices are being updated for patch 0.13.5! 🌟 If you enjoy using tarkov.dev, please consider donating to help keep it running. All donations go directly towards server costs and operational expenses. This banner will only be up for a week. Thank you! ❤️',
 
     linkEnabled: true,
     linkText: 'Donate',
