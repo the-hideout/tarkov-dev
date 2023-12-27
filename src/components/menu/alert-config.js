69dc249f91262df8a3c3c415acc2a3af7f2eace4
Thaddeus Bond <thaddeus@thaddeus.io>
2023-12-27 06:03:49 +0000

    Add scanner leveling banner

--- src/components/menu/alert-config.js
+++ src/components/menu/alert-config.js
@@ -1,6 +1,6 @@
 const alertConfig = {
     // set this bool if the site alert should be enabled or not
-    alertEnabled: false,
+    alertEnabled: true,
 
     // valid alert colors
     alertColors: {
@@ -11,19 +11,19 @@ const alertConfig = {
     },
 
     // set this variable to the severity of the alert banner
-    alertLevel: 'info',
+    alertLevel: 'warning',
 
     // The text to display in the alert banner
-    text: '🌟 Flea market scanners have been levelled and flea market prices are being updated for patch 0.13.5! 🌟 If you enjoy using tarkov.dev, please consider donating to help keep it running. All donations go directly towards server costs and operational expenses. This banner will only be up for a week. Thank you! ❤️',
+    text: 'Flea market scanners are currently being leveled, and data for the 0.14.0.0 wipe should be appearing soon!',
 
-    linkEnabled: true,
+    linkEnabled: false,
     linkText: 'Donate',
     link: 'https://opencollective.com/tarkov-dev',
 
     // when a banner with a specific key is hidden, it never shows for that user again
     // (unless they clear their browser cache)
     // use a different key to force new banners to display again
-    bannerKey: 'alertBanner-0.13.5-flea'
+    bannerKey: 'alertBanner-0.14.0-flea'
 }
 
 export default alertConfig
