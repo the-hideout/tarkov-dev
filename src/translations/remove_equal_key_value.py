1285ce805ef9e83a34a665370810a43a406103fc
Shebuka <shebuka@hotmail.com>
2024-02-05 16:32:13 +0100

    Updated script to remove equal key values for all namespace files too

--- src/translations/remove_equal_key_value.py
+++ src/translations/remove_equal_key_value.py
@@ -17,16 +17,26 @@ output_dir = 'poeditor'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# Read JSON data from file
-with open(os.path.join(translations_dir, 'en', 'translation.json'), encoding='utf-8') as json_file:
-    data_en = json.load(json_file)
 
-for lang in languages:
-    for file in os.listdir(os.path.join(translations_dir, lang)):
-        if file.endswith('translation.json'):
+for en_file_name in os.listdir(os.path.join(translations_dir, 'en')):
+    en_file_path = os.path.join(translations_dir, 'en', en_file_name)
+    
+    # Read JSON data from file
+    with open(en_file_path, encoding='utf-8') as json_en_file:
+        data_en = json.load(json_en_file)
+    
+    for lang in languages:
+        lang_file_path = os.path.join(translations_dir, lang, en_file_name)
+
+        # If the file doesn't exist in the language folder, copy it from 'en' folder
+        if not os.path.exists(lang_file_path):
+            #copyfile(en_file_path, lang_file_path)
+            #print(f"File '{en_file_name}' copied to '{lang}' folder.")
+            print(f"File '{en_file_name}' is not present in '{lang}' folder.")
+        else:
             # Read JSON data from file
-            with open(os.path.join(translations_dir, lang, file), encoding='utf-8') as json_file:
-                data = json.load(json_file)
+            with open(lang_file_path, encoding='utf-8') as json_lang_file:
+                data = json.load(json_lang_file)
 
             # remove key where key == value
             for key in list(data.keys()):
@@ -40,6 +50,7 @@ for lang in languages:
                     data[key] = ''
 
             # Write the modified data to the output folder with language appended to the file name
-            new_file = file.replace('.json', f'_{lang}.json')
+            new_file = en_file_name.replace('.json', f'_{lang}.json')
             with open(os.path.join(output_dir, new_file), 'w', encoding='utf-8') as json_file:
                 json.dump(data, json_file, ensure_ascii=False, indent=4)
+                print(f"File '{en_file_name}' cleaned from English values and saved as '{new_file}'.")
