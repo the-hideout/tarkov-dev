d12e35da94a706fbcef259fb2cddddea515a21d3
Razzmatazz <razz@badkarma.xyz>
2024-02-05 15:31:10 -0600

    Exclude flea presets from trader spending table

--- src/components/small-item-table/index.js
+++ src/components/small-item-table/index.js
@@ -352,6 +352,8 @@ function SmallItemTable(props) {
                         return false;
                     if (!showAllSources && settings.useTarkovTracker && buyFor.vendor.taskUnlock && !settings.completedQuests.includes(buyFor.vendor.taskUnlock.id)) 
                         return false;
+                    if (buyFor.vendor.normalizedName === 'flea-market' && traderValue && traderBuyback && itemData.types.includes('preset'))
+                        return false;
                     return true;
                 }),
                 sellFor: itemData.sellFor,
