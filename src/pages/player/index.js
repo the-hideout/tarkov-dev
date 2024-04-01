d636625c307e7829f8f35a8d54e99a2b974cb829
Razzmatazz <razz@badkarma.xyz>
2024-04-01 13:25:11 -0500

    show member flags

--- src/pages/player/index.js
+++ src/pages/player/index.js
@@ -64,6 +64,13 @@ const raritySort = {
     "legendary": 2
 }
 
+const memberFlags = {
+    Developer: 1,
+    EOD: 2,
+    Sherpa: 256,
+    Emissary: 512,
+}
+
 function Player() {
     const turnstileRef = useRef();
     const inputFile = useRef() 
@@ -206,6 +213,20 @@ function Player() {
             return t('Banned Permanently');
         }
         return t('Banned until {{banLiftDate}}', { banLiftDate: new Date(playerData.info.bannedUntil * 1000).toLocaleString() });
+    }, [playerData, t]);
+
+    const accountCategories = useMemo(() => {
+        if (!playerData?.info?.memberCategory) {
+            return '';
+        }
+        const flags = [];
+        for (const flagName of Object.keys(memberFlags)) {
+            const flag = memberFlags[flagName];
+            if (playerData.info.memberCategory & flag) {
+                flags.push(t(flagName));
+            }
+        }
+        return <div>{flags.join(', ')}</div>;
     }, [playerData, t]);
 
     const achievementColumns = useMemo(
@@ -883,6 +904,7 @@ function Player() {
                 />
             </div>
             <div>
+                {accountCategories}
                 {!!playerData.saved && (
                     <p className="banned">{t('Warning: Profiles loaded from files may contain edited information')}</p>
                 )}
