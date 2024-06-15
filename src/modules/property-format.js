dd67487411b24223a13ba68e547b3d2685cafe9d
Shebuka <shebuka@hotmail.com>
2024-06-15 20:09:08 +0200

    eslint fix

--- src/modules/property-format.js
+++ src/modules/property-format.js
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import { isValidElement } from 'react';
 
-import camelcaseToDashes from './camelcase-to-dashes.js';
 import { formatCaliber } from './format-ammo.mjs';
 
 import i18n from '../i18n.js';
