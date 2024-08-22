16809d8594e169383d8da5a1dfef9023edfb7492
Razzmatazz <razz@badkarma.xyz>
2024-08-21 19:34:29 -0500

    update banner

--- src/components/menu/alert-config.js
+++ src/components/menu/alert-config.js
@@ -14,8 +14,8 @@ const alertConfig = {
     alertLevel: 'info',
 
     // The text to display in the alert banner
-    text: '🌟 Flea market scanners are currently being leveled, and data for the {{patchVersion}} patch should be appearing soon! 🌟 If you enjoy using tarkov.dev, please consider donating to help keep it running. All donations go directly towards server costs and operational expenses. This banner will only be up for a week. Thank you! ❤️',
-    //text: '🌟 Flea market scanners have been leveled, and flea market prices are being updated for patch {{patchVersion}}! 🌟 If you enjoy using tarkov.dev, please consider donating to help keep it running. All donations go directly towards server costs and operational expenses. This banner will only be up for a week. Thank you! ❤️',
+    //text: '🌟 Flea market scanners are currently being leveled, and data for the {{patchVersion}} patch should be appearing soon! 🌟 If you enjoy using tarkov.dev, please consider donating to help keep it running. All donations go directly towards server costs and operational expenses. This banner will only be up for a week. Thank you! ❤️',
+    text: '🌟 Flea market scanners have been leveled, and flea market prices are being updated for patch {{patchVersion}}! 🌟 If you enjoy using tarkov.dev, please consider donating to help keep it running. All donations go directly towards server costs and operational expenses. This banner will only be up for a week. Thank you! ❤️',
     textVariables: {patchVersion: '0.15.0'},
     linkEnabled: true,
     linkText: 'Donate',
@@ -24,7 +24,7 @@ const alertConfig = {
     // when a banner with a specific key is hidden, it never shows for that user again
     // (unless they clear their browser cache)
     // use a different key to force new banners to display again
-    bannerKey: 'alertBanner-0.15.0-wipe'
+    bannerKey: 'alertBanner-0.15.0-scanning'
 }
 
 export default alertConfig
