8f41bf79520570738f3e5fcbb00227bc67cd1faf
Razzmatazz <razz@badkarma.xyz>
2024-09-19 16:29:16 -0500

    Add button to check if player is banned

--- src/pages/player/player-forward.js
+++ src/pages/player/player-forward.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
 function PlayerForward() {
     const params = useParams();
     const navigate = useNavigate();
-    navigate('/players/regular/'+params.accountId);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            navigate('/players/regular/'+params.accountId);
+        }, 500);
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [params, navigate]);
 }
 
 export default PlayerForward;
