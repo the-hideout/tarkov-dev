00f4ec51b0c5135a079de02cf7927f42df7961fb
Allan <workofcode@gmail.com>
2025-01-07 19:35:35 +0000

    Fix: alert config unable to read window.globals, reverted

--- src/components/menu/alert-config.js
+++ src/components/menu/alert-config.js
@@ -4,10 +4,10 @@ const alertConfig = {
 
     // valid alert colors
     alertColors: {
-        error: window.globalColors.colorActionNegative,
-        info: window.globalColors.colorBlueLight,
-        success: window.globalColors.colorActionPositive,
-        warning: window.globalColors.colorActionWarning,
+      error: '#cd1e2f',
+      info: '#0292c0',
+      success: '#00a700',
+      warning: '#ca8a00',
     },
 
     // set this variable to the severity of the alert banner
