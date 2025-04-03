ba9f5a10f9d927abbe4c8f1ecabffa0ac6bae9f7
Veit F. <veitfraenzer96@hotmail.de>
2025-04-03 22:26:30 +0200

    perf: :zap: Added a debounce to the control-quest-search input event emitter
    This should improve performance, since less computation needs to be done

--- src/modules/leaflet-control-quest-search.js
+++ src/modules/leaflet-control-quest-search.js
@@ -12,7 +12,7 @@ L.Control.QuestSearch = L.Control.extend({
         wrapper.style.margin = '10px 0px';
 
         const searchBar = L.DomUtil.create('input');
-        searchBar.id = "map-search-bar";
+        searchBar.id = 'map-search-bar';
         searchBar.setAttribute('type', 'text');
         searchBar.setAttribute(
             'placeholder',
@@ -29,79 +29,82 @@ L.Control.QuestSearch = L.Control.extend({
         // Prevent zooming of the map by double clicking the search field
         searchBar.addEventListener('dblclick', (e) => {
             e.stopPropagation();
-        })
+        });
 
         // Reset all markers
-        searchBar.addEventListener('input', (e) => {
-            if (e.target.value.length < 2) {
-                if (e.target.value.length === 0) {
-                    markers.allMarkers.forEach((marker) => {
-                        if ('getElement' in marker && !('_bounds' in marker)) {
-                            marker.getElement().classList.remove('not-shown');
-                            marker.getElement().classList.remove('pulse');
-                        }
-                    });
-                }
-                return;
-            }
-
-            // Reassign all markers to prevent layer toggles leading to bugs while toggling layers after a search
-            markers.allMarkers = Object.values(map._targets);
-
-            // #region Quest searching            
-            const foundQuest = this.options.quests.filter((quest) => {
-                return quest.name.toLowerCase().includes(e.target.value.toLowerCase());
-            });
-
-            const { objectiveMarkers, nonObjectiveMarkers } = markers.allMarkers.reduce(
-                (acc, marker) => {
-                    if (foundQuest.some((quest) => quest.id === marker.options.questId)) {
-                        acc.objectiveMarkers.push(marker);
-                    } else {
-                        acc.nonObjectiveMarkers.push(marker);
+        searchBar.addEventListener(
+            'input',
+            debounce((e) => {
+                if (e.target.value.length < 2) {
+                    if (e.target.value.length === 0) {
+                        markers.allMarkers.forEach((marker) => {
+                            if ('getElement' in marker && !('_bounds' in marker)) {
+                                marker.getElement().classList.remove('not-shown');
+                                marker.getElement().classList.remove('pulse');
+                            }
+                        });
                     }
-
-                    return acc;
-                },
-                { objectiveMarkers: [], nonObjectiveMarkers: [] },
-            );
-
-            markers.objectiveMarkers = objectiveMarkers;
-            markers.nonObjectiveMarkers = nonObjectiveMarkers;
-            markers.objectiveMarkers.forEach((marker) => {
-                if ('getElement' in marker) {
-                    marker.getElement().classList.add('pulse');
-                    marker.getElement().classList.remove('not-shown');
+                    return;
                 }
-            });
 
-            markers.nonObjectiveMarkers.forEach((marker) => {
-                if ('getElement' in marker) {
-                    marker.getElement().classList.add('not-shown');
-                    marker.getElement().classList.remove('pulse');
-                }
-            });
-            // #endregion
+                // Reassign all markers to prevent layer toggles leading to bugs while toggling layers after a search
+                markers.allMarkers = Object.values(map._targets);
 
-            // #region Item, Containers and general searching (Should also show e.g. Suitcases, Corpses etc.)
-            markers.itemAndContainerMarkers = [
-                ...markers.allMarkers.filter((marker) =>
-                    marker.options.title?.toLowerCase().includes(e.target.value.toLowerCase()),
-                ),
-                ...markers.allMarkers.filter((marker) =>
-                    marker.options?.items?.some((item) =>
-                        item.toLowerCase().includes(e.target.value.toLowerCase()),
+                // #region Quest searching
+                const foundQuest = this.options.quests.filter((quest) => {
+                    return quest.name.toLowerCase().includes(e.target.value.toLowerCase());
+                });
+
+                const { objectiveMarkers, nonObjectiveMarkers } = markers.allMarkers.reduce(
+                    (acc, marker) => {
+                        if (foundQuest.some((quest) => quest.id === marker.options.questId)) {
+                            acc.objectiveMarkers.push(marker);
+                        } else {
+                            acc.nonObjectiveMarkers.push(marker);
+                        }
+
+                        return acc;
+                    },
+                    { objectiveMarkers: [], nonObjectiveMarkers: [] },
+                );
+
+                markers.objectiveMarkers = objectiveMarkers;
+                markers.nonObjectiveMarkers = nonObjectiveMarkers;
+                markers.objectiveMarkers.forEach((marker) => {
+                    if ('getElement' in marker) {
+                        marker.getElement().classList.add('pulse');
+                        marker.getElement().classList.remove('not-shown');
+                    }
+                });
+
+                markers.nonObjectiveMarkers.forEach((marker) => {
+                    if ('getElement' in marker) {
+                        marker.getElement().classList.add('not-shown');
+                        marker.getElement().classList.remove('pulse');
+                    }
+                });
+                // #endregion
+
+                // #region Item, Containers and general searching (Should also show e.g. Suitcases, Corpses etc.)
+                markers.itemAndContainerMarkers = [
+                    ...markers.allMarkers.filter((marker) =>
+                        marker.options.title?.toLowerCase().includes(e.target.value.toLowerCase()),
                     ),
-                ),
-            ];
+                    ...markers.allMarkers.filter((marker) =>
+                        marker.options?.items?.some((item) =>
+                            item.toLowerCase().includes(e.target.value.toLowerCase()),
+                        ),
+                    ),
+                ];
 
-            markers.itemAndContainerMarkers.forEach((marker) => {
-                if ('getElement' in marker) {
-                    marker.getElement().classList.add('pulse');
-                    marker.getElement().classList.remove('not-shown');
-                }
-            });
-        });
+                markers.itemAndContainerMarkers.forEach((marker) => {
+                    if ('getElement' in marker) {
+                        marker.getElement().classList.add('pulse');
+                        marker.getElement().classList.remove('not-shown');
+                    }
+                });
+            }, 300)
+        );
         // #endregion
 
         wrapper.append(searchBar);
@@ -112,3 +115,15 @@ L.Control.QuestSearch = L.Control.extend({
 L.control.questSearch = function (opts) {
     return new L.Control.QuestSearch(opts);
 };
+
+// #region Helper functions
+function debounce(func, delay) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+// #endregion
