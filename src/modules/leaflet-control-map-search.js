ef50134e05b0e84abb75a16b3c2e9cb28dfd6c6f
Veit F. <veitfraenzer96@hotmail.de>
2025-04-04 07:45:18 +0200

    style: :art: corrected indentions, removed unnecessary comments

--- src/modules/leaflet-control-map-search.js
+++ src/modules/leaflet-control-map-search.js
@@ -1,15 +1,18 @@
 import L from 'leaflet';
 
-// POC: This is just a proof of concept
 L.Control.MapSearch = L.Control.extend({
     options: {
         quests: null,
         placeholderText: null,
-        descriptionText: null
+        descriptionText: null,
     },
     onAdd: function (map) {
         const wrapper = L.DomUtil.create('div');
-        wrapper.classList.add('search-wrapper', 'leaflet-control-icon-search', 'maps-search-wrapper');
+        wrapper.classList.add(
+            'search-wrapper',
+            'leaflet-control-icon-search',
+            'maps-search-wrapper',
+        );
 
         const info = L.DomUtil.create('div');
         info.classList.add('maps-search-wrapper-info');
@@ -39,13 +42,13 @@ L.Control.MapSearch = L.Control.extend({
             'input',
             debounce((e) => {
                 const inputValue = e.target.value.trim();
-        
+
                 // Split the input into multiple search terms and filter out empty strings
                 const searchTerms = inputValue
                     .split(',')
                     .map((term) => term.trim().toLowerCase())
                     .filter((term) => term.length > 0);
-        
+
                 if (searchTerms.length === 0) {
                     // Reset markers if no valid search terms are provided
                     markers.allMarkers.forEach((marker) => {
@@ -56,15 +59,15 @@ L.Control.MapSearch = L.Control.extend({
                     });
                     return;
                 }
-        
+
                 // Reassign all markers to prevent layer toggles leading to bugs while toggling layers after a search
                 markers.allMarkers = Object.values(map._targets);
-        
+
                 // #region Quest Searching
                 const foundQuest = this.options.quests.filter((quest) =>
-                    searchTerms.some((term) => quest.name.toLowerCase().includes(term))
+                    searchTerms.some((term) => quest.name.toLowerCase().includes(term)),
                 );
-        
+
                 const { objectiveMarkers, nonObjectiveMarkers } = markers.allMarkers.reduce(
                     (acc, marker) => {
                         if (foundQuest.some((quest) => quest.id === marker.options.questId)) {
@@ -72,22 +75,22 @@ L.Control.MapSearch = L.Control.extend({
                         } else {
                             acc.nonObjectiveMarkers.push(marker);
                         }
-        
+
                         return acc;
                     },
-                    { objectiveMarkers: [], nonObjectiveMarkers: [] }
+                    { objectiveMarkers: [], nonObjectiveMarkers: [] },
                 );
-        
+
                 markers.objectiveMarkers = objectiveMarkers;
                 markers.nonObjectiveMarkers = nonObjectiveMarkers;
-        
+
                 markers.objectiveMarkers.forEach((marker) => {
                     if ('getElement' in marker) {
                         marker.getElement().classList.add('pulse');
                         marker.getElement().classList.remove('not-shown');
                     }
                 });
-        
+
                 markers.nonObjectiveMarkers.forEach((marker) => {
                     if ('getElement' in marker) {
                         marker.getElement().classList.add('not-shown');
@@ -95,23 +98,23 @@ L.Control.MapSearch = L.Control.extend({
                     }
                 });
                 // #endregion
-        
+
                 // #region Item, Containers, and General Searching
                 markers.itemAndContainerMarkers = [
                     ...markers.allMarkers.filter((marker) =>
                         searchTerms.some((term) =>
-                            marker.options.title?.toLowerCase().includes(term)
-                        )
+                            marker.options.title?.toLowerCase().includes(term),
+                        ),
                     ),
                     ...markers.allMarkers.filter((marker) =>
                         searchTerms.some((term) =>
                             marker.options?.items?.some((item) =>
-                                item.toLowerCase().includes(term)
-                            )
-                        )
+                                item.toLowerCase().includes(term),
+                            ),
+                        ),
                     ),
                 ];
-        
+
                 markers.itemAndContainerMarkers.forEach((marker) => {
                     if ('getElement' in marker) {
                         marker.getElement().classList.add('pulse');
@@ -119,9 +122,8 @@ L.Control.MapSearch = L.Control.extend({
                     }
                 });
                 // #endregion
-            }, 300)
+            }, 300),
         );
-          
 
         wrapper.append(searchBar);
         wrapper.append(info);
