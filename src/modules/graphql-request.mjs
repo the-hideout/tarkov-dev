09111736703056e8f002636b93891cbdaebe6fea
Razzmatazz <razz@badkarma.xyz>
2025-05-22 14:48:54 -0500

    limit OPTIONS requests

--- src/modules/graphql-request.mjs
+++ src/modules/graphql-request.mjs
@@ -13,7 +13,7 @@ export default async function graphqlRequest(queryString, variables) {
     return fetch(apiUrl, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
+            //'Content-Type': 'application/json', // setting the Content-Type header prevents this from being a 'simple' request and results in an additional preflight OPTIONS request
             Accept: 'application/json',
         },
         body: JSON.stringify({
