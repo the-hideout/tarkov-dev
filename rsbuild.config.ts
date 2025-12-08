5e701486022e71394d68d22639b2a3b046533bb8
JiPai <github@jipai.moe>
2025-12-09 05:32:14 +0800

    feat(version): use Git version replace API fetch version

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,23 +1,31 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSvgr } from "@rsbuild/plugin-svgr";
+import { execSync } from "node:child_process";
+
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+const branchName = execSync("git rev-parse --abbrev-ref HEAD")
+  .toString()
+  .trim();
 
 export default defineConfig({
-    plugins: [pluginReact(), pluginSvgr({ mixedImport: true })],
-    server: {
-        base: process.env.PUBLIC_URL,
+  plugins: [pluginReact(), pluginSvgr({ mixedImport: true })],
+  server: {
+    base: process.env.PUBLIC_URL,
+  },
+  source: {
+    define: {
+      "process.env.RSTEST": process.env.RSTEST,
+      __COMMIT_HASH__: JSON.stringify(commitHash),
+      __BRANCH_NAME__: JSON.stringify(branchName),
     },
-    source: {
-        define: {
-            'process.env.RSTEST': process.env.RSTEST,
-        },
+  },
+  html: {
+    template: "./public/index.html",
+  },
+  output: {
+    distPath: {
+      root: "build",
     },
-    html: {
-        template: './public/index.html',
-    },
-    output: {
-        distPath: {
-            root: 'build',
-        },
-    },
+  },
 });
