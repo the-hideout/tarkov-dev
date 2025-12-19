c11d936c6ca68a7cf99ae9979c5458b28ce14b25
Shebuka <shebuka@hotmail.com>
2025-12-19 18:06:08 +0100

    Added stylelint and fixed prettier and i18next parser configs

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    printWidth: 200,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+};
+
+export default config;
