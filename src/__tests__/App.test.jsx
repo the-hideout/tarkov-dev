52c5e056350521002142fa8bf180ac28428d0425
Shebuka <shebuka@hotmail.com>
2026-01-07 13:41:50 +0100

    Prettier src/__tests__; (Manual pass of `npx prettier "**/*.{js,jsx,mjs}" --write --ignore-unknown`)

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,26 +1,24 @@
-import { expect, it } from '@rstest/core';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { act, waitFor, screen } from '@testing-library/react';
-import React from 'react';
-import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils';
+import { expect, it } from "@rstest/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { waitFor, screen } from "@testing-library/react";
+import React from "react";
+import { mockAllIsIntersecting } from "react-intersection-observer/test-utils";
 
-import App from '#src/App.jsx';
-import { renderWithProviders } from '#src/__tests__/test-utils.js';
+import App from "#src/App.jsx";
+import { renderWithProviders } from "#src/__tests__/test-utils.js";
 
 const queryClient = new QueryClient();
 
-it('renders without crashing', async () => {
+it("renders without crashing", async () => {
     mockAllIsIntersecting(true);
 
-    await act(async () => {
-        renderWithProviders(
-            <QueryClientProvider client={queryClient}>
-                <App />
-            </QueryClientProvider>,
-        );
-    });
+    renderWithProviders(
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>,
+    );
 
     await waitFor(() => {
-        expect(screen.getByRole('navigation')).toHaveClass('navigation');
+        expect(screen.getByRole("navigation")).toHaveClass("navigation");
     });
 });
