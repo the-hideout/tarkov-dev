ae939bcb72e66b2683c0524ac126e11fa2b085e3
JiPai <github@jipai.moe>
2026-01-16 00:17:42 +0800

    feat(table): fix table head sort icon not align in left style

---
+++ src/components/data-table/TableHead.tsx
@@ -0,0 +1,50 @@
+import clsx from "clsx";
+import ArrowIcon from "#src/components/data-table/Arrow.jsx";
+import { TableHeaderProps } from "react-table";
+
+import "./index.css";
+
+export default function TableHead({
+    itemKey,
+    headProps,
+    children,
+    align = "center",
+    isSorted,
+    isSortedDesc,
+}: {
+    itemKey: string;
+    children: React.ReactNode;
+    align: "left" | "center" | "right";
+    isSorted?: boolean;
+    isSortedDesc?: boolean;
+    headProps: TableHeaderProps;
+}) {
+    return (
+        <th
+            {...headProps}
+            key={itemKey}
+            className={clsx(headProps.className, {
+                "table-head--left": align === "left",
+                "table-head--right": align === "right",
+            })}
+        >
+            <span>{children}</span>
+
+            <i
+                className={clsx(
+                    "header-sort-icon-container",
+                    !isSorted && "header-sort-icon-container--h-0",
+                    align === "center" ? "header-sort-icon-container--block" : "header-sort-icon-container--ml-6",
+                )}
+            >
+                <ArrowIcon
+                    className={clsx(
+                        "header-sort-icon__icon",
+                        isSortedDesc ? "header-sort-icon__icon--down" : "header-sort-icon__icon--up",
+                        isSorted ? "header-sort-icon__icon--visible" : "header-sort-icon__icon--hidden",
+                    )}
+                />
+            </i>
+        </th>
+    );
+}
