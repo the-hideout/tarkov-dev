98475554566cce60afcd9aaca86b6bcf473aa269
JiPai <github@jipai.moe>
2026-06-19 17:37:42 +0800

    chore(eslint): upgrade eslint8 to eslint10

---
+++ eslint.config.ts
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintReact from "@eslint-react/eslint-plugin";
+import eslintJs from "@eslint/js";
+
+export default defineConfig([
+    {
+        ignores: [
+            "build/**",
+            "dist/**",
+            "eslint.config.ts",
+            "prettier.config.mjs",
+            "stylelint.config.mjs",
+            "rsbuild.config.ts",
+            "rstest.config.ts",
+            "rstest.setup.ts",
+            "i18next-parser.config.mjs",
+        ],
+    },
+    {
+        files: ["src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}", "scripts/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        plugins: { js },
+        extends: [
+            eslintJs.configs.recommended,
+            tseslint.configs.recommended,
+            eslintReact.configs["recommended-typescript"],
+        ],
+
+        languageOptions: {
+            globals: { ...globals.browser, process: "readonly" },
+            parser: tseslint.parser,
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@eslint-react/no-missing-key": "warn",
+            "no-useless-assignment": "warn",
+        },
+    },
+]);
